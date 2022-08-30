9f82a13569cb4c4eb55a4b4e54b52f9be4aca91f
Impulse21 <dipaolo21@gmail.com>
2022-08-29 23:49:09 -0400

    Getting the scene rendering again - losts to revisit.

--- PhxEngine-Editor/Src/SceneRenderLayer.cpp
+++ PhxEngine-Editor/Src/SceneRenderLayer.cpp
@@ -1,14 +1,21 @@
 #include "SceneRenderLayer.h"
 #include <PhxEngine/Graphics/ShaderStore.h>
+#include <PhxEngine/Core/Span.h>
+#include <Shaders/ShaderInteropStructures.h>
+#include "DeferredSceneRenderer.h"
 
 using namespace PhxEngine;
+using namespace PhxEngine::Core;
 using namespace PhxEngine::Scene;
 
 using namespace PhxEngine::RHI;
 
+#define USE_DUMMY_RENDERER 1
+
 namespace
 {
-    class DeferredRenderer : public Graphics::IRenderer
+
+    class DummyRenderer : public PhxEngine::Graphics::IRenderer
     {
         enum PsoType
         {
@@ -20,181 +27,14 @@ namespace
         };
 
     public:
-        void Initialize() override;
-        void Finialize() override
-        {
-            this->FreeTextureResources();
-        }
+        void Initialize() override {}
+        void Finialize() override {}
+        void RenderScene(PhxEngine::Scene::New::CameraComponent const& camera, PhxEngine::Scene::New::Scene const& scene) {}
 
-        void RenderScene(Scene::New::CameraComponent const& camera, Scene::New::Scene const& scene);
+        PhxEngine::RHI::TextureHandle GetFinalColourBuffer() override { return TextureHandle(); }
 
-        RHI::TextureHandle GetFinalColourBuffer() override { return this->m_deferredLightBuffer; }
-
-        void OnWindowResize(DirectX::XMFLOAT2 const& size) override
-        {
-            this->FreeTextureResources();
-
-            this->CreateRenderTargets(size);
-        }
-
-    private:
-        struct GBuffer
-        {
-            PhxEngine::RHI::TextureHandle AlbedoTexture;
-            PhxEngine::RHI::TextureHandle NormalTexture;
-            PhxEngine::RHI::TextureHandle SurfaceTexture;
-
-            PhxEngine::RHI::TextureHandle _PostionTexture;
-        };
-
-        void FreeTextureResources()
-        {
-            RHI::IGraphicsDevice::Ptr->FreeTexture(this->m_depthBuffer);
-            RHI::IGraphicsDevice::Ptr->FreeTexture(this->m_gBuffer.AlbedoTexture);
-            RHI::IGraphicsDevice::Ptr->FreeTexture(this->m_gBuffer.NormalTexture);
-            RHI::IGraphicsDevice::Ptr->FreeTexture(this->m_gBuffer.SurfaceTexture);
-            RHI::IGraphicsDevice::Ptr->FreeTexture(this->m_gBuffer._PostionTexture);
-            RHI::IGraphicsDevice::Ptr->FreeTexture(this->m_deferredLightBuffer);
-        }
-
-        void CreatePSOs();
-        void CreateRenderTargets(DirectX::XMFLOAT2 const& size);
-
-    private:
-        RHI::CommandListHandle m_commandList;
-        RHI::CommandListHandle m_computeCommandList;
-
-        // -- Scene CPU Buffers ---
-        std::vector<Shader::Geometry> m_geometryCpuData;
-        std::vector<Shader::MaterialData> m_materialCpuData;
-
-        // Uploaded every frame....Could be improved upon.
-        std::vector<Shader::ShaderLight> m_lightCpuData;
-        std::vector<Shader::ShaderLight> m_shadowLights;
-        std::vector<DirectX::XMFLOAT4X4> m_matricesCPUData;
-
-        std::array<GraphicsPSOHandle, PsoType::NumPsoTypes> m_pso;
-
-        GBuffer m_gBuffer;
-        PhxEngine::RHI::TextureHandle m_deferredLightBuffer;
-        PhxEngine::RHI::TextureHandle m_depthBuffer;
+        void OnWindowResize(DirectX::XMFLOAT2 const& size) override {};
     };
-
-
-    void DeferredRenderer::Initialize()
-    {
-        auto& spec = LayeredApplication::Ptr->GetSpec();
-        this->CreatePSOs();
-        this->CreateRenderTargets({ static_cast<float>(spec.WindowWidth), static_cast<float>(spec.WindowHeight) });
-
-        this->m_commandList = RHI::IGraphicsDevice::Ptr->CreateCommandList();
-        RHI::CommandListDesc commandLineDesc = {};
-        commandLineDesc.QueueType = RHI::CommandQueueType::Compute;
-        this->m_computeCommandList = RHI::IGraphicsDevice::Ptr->CreateCommandList(commandLineDesc);
-    }
-
-    void DeferredRenderer::CreateRenderTargets(DirectX::XMFLOAT2 const& size)
-    {
-        // -- Depth ---
-        RHI::TextureDesc desc = {};
-        desc.Width = std::max(size.x, 1.0f);
-        desc.Height = std::max(size.y, 1.0f);
-        desc.Dimension = RHI::TextureDimension::Texture2D;
-        desc.IsBindless = false;
-
-        desc.Format = RHI::FormatType::D32;
-        desc.IsTypeless = true;
-        desc.DebugName = "Depth Buffer";
-        desc.OptmizedClearValue.DepthStencil.Depth = 1.0f;
-        desc.BindingFlags = desc.BindingFlags | RHI::BindingFlags::DepthStencil;
-        desc.InitialState = RHI::ResourceStates::DepthWrite;
-
-        this->m_depthBuffer = RHI::IGraphicsDevice::Ptr->CreateTexture(desc);
-
-        // -- GBuffers ---
-        desc.BindingFlags = RHI::BindingFlags::RenderTarget | RHI::BindingFlags::ShaderResource;
-        desc.InitialState = RHI::ResourceStates::RenderTarget;
-
-        desc.Format = RHI::FormatType::RGBA32_FLOAT;
-        desc.DebugName = "Albedo Buffer";
-
-        this->m_gBuffer.AlbedoTexture = RHI::IGraphicsDevice::Ptr->CreateTexture(desc);
-
-        // desc.Format = RHI::FormatType::R10G10B10A2_UNORM;
-        desc.Format = RHI::FormatType::RGBA16_SNORM;
-        desc.DebugName = "Normal Buffer";
-        this->m_gBuffer.NormalTexture = RHI::IGraphicsDevice::Ptr->CreateTexture(desc);
-
-
-        desc.Format = RHI::FormatType::RGBA8_UNORM;
-        desc.DebugName = "Surface Buffer";
-        this->m_gBuffer.SurfaceTexture = RHI::IGraphicsDevice::Ptr->CreateTexture(desc);
-
-        desc.Format = RHI::FormatType::RGBA32_FLOAT;
-        desc.DebugName = "Debug Position Buffer";
-
-        // TODO: Determine what format should be used here.
-        desc.Format = RHI::FormatType::R10G10B10A2_UNORM;
-        desc.DebugName = "Deferred Lighting";
-        this->m_deferredLightBuffer = RHI::IGraphicsDevice::Ptr->CreateTexture(desc);
-    }
-
-    void DeferredRenderer::CreatePSOs()
-    {
-        {
-            RHI::GraphicsPSODesc psoDesc = {};
-            psoDesc.VertexShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::VS_GBufferPass);
-            psoDesc.PixelShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::PS_GBufferPass);
-            psoDesc.InputLayout = nullptr;
-
-            // psoDesc.RasterRenderState.CullMode = RHI::RasterCullMode::None;
-            psoDesc.RtvFormats.push_back(IGraphicsDevice::Ptr->GetTextureDesc(this->m_gBuffer.AlbedoTexture).Format);
-            psoDesc.RtvFormats.push_back(IGraphicsDevice::Ptr->GetTextureDesc(this->m_gBuffer.NormalTexture).Format);
-            psoDesc.RtvFormats.push_back(IGraphicsDevice::Ptr->GetTextureDesc(this->m_gBuffer.SurfaceTexture).Format);
-            psoDesc.RtvFormats.push_back(IGraphicsDevice::Ptr->GetTextureDesc(this->m_gBuffer._PostionTexture).Format);
-            psoDesc.DsvFormat = IGraphicsDevice::Ptr->GetTextureDesc(this->m_depthBuffer).Format;
-
-            this->m_pso[PSO_GBufferPass] = IGraphicsDevice::Ptr->CreateGraphicsPSO(psoDesc);
-        }
-
-        {
-            /*
-            RHI::GraphicsPSODesc psoDesc = {};
-            psoDesc.VertexShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::VS_ShadowPass);
-            psoDesc.InputLayout = nullptr;
-
-            psoDesc.DsvFormat = kShadowAtlasFormat;
-            psoDesc.RasterRenderState.DepthBias = 100000;
-            psoDesc.RasterRenderState.DepthBiasClamp = 0.0f;
-            psoDesc.RasterRenderState.SlopeScaledDepthBias = 1.0f;
-            psoDesc.RasterRenderState.DepthClipEnable = false;
-
-            this->m_pso[PSO_] = IGraphicsDevice::Ptr->CreateGraphicsPSO(psoDesc);
-            */
-        }
-
-        {
-            // TODO: Use the files directly
-            RHI::GraphicsPSODesc psoDesc = {};
-            psoDesc.VertexShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::VS_FullscreenQuad);
-            psoDesc.PixelShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::PS_FullscreenQuad);
-            psoDesc.InputLayout = nullptr;
-            psoDesc.RasterRenderState.CullMode = RHI::RasterCullMode::Front;
-            psoDesc.DepthStencilRenderState.DepthTestEnable = false;
-            psoDesc.RtvFormats.push_back(IGraphicsDevice::Ptr->GetTextureDesc(IGraphicsDevice::Ptr->GetBackBuffer()).Format);
-            this->m_pso[PSO_FullScreenQuad] = IGraphicsDevice::Ptr->CreateGraphicsPSO(psoDesc);
-
-            psoDesc.VertexShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::VS_DeferredLighting);
-            psoDesc.PixelShader = Graphics::ShaderStore::Ptr->Retrieve(Graphics::PreLoadShaders::PS_DeferredLighting);
-
-            this->m_pso[PSO_DeferredLightingPass] = IGraphicsDevice::Ptr->CreateGraphicsPSO(psoDesc);
-        }
-    }
-
-    void DeferredRenderer::RenderScene(Scene::New::CameraComponent const& camera, Scene::New::Scene const& scene)
-    {
-
-    }
 }
 
 // ----------------------------------------------------------------------------
@@ -202,7 +42,11 @@ namespace
 SceneRenderLayer::SceneRenderLayer()
 	: AppLayer("Scene Render Layer")
 {
+#ifdef USE_DUMMY_RENDERER
+    this->m_sceneRenderer = std::make_unique<DummyRenderer>();
+#else
     this->m_sceneRenderer = std::make_unique<DeferredRenderer>();
+#endif
 }
 
 void SceneRenderLayer::OnAttach()
--- Shaders/Include/Shaders/ShaderInteropStructures.h
+++ Shaders/Include/Shaders/ShaderInteropStructures.h
@@ -11,263 +11,327 @@ namespace Shader
 {
 #endif
 
-// -- Enums ---
-static const uint ENTITY_TYPE_DIRECTIONALLIGHT = 0;
-static const uint ENTITY_TYPE_OMNILIGHT = 1;
-static const uint ENTITY_TYPE_SPOTLIGHT = 2;
+	// -- Enums ---
+	static const uint ENTITY_TYPE_DIRECTIONALLIGHT = 0;
+	static const uint ENTITY_TYPE_OMNILIGHT = 1;
+	static const uint ENTITY_TYPE_SPOTLIGHT = 2;
 
-struct ShaderLight
-{
-	float3 Position;
-	uint Type8_Flags8_Range16; // <Range_16><flags_8><type_8>
+	static const uint SHADER_LIGHT_ENTITY_COUNT = 256;
 
-	// -- 16 byte boundary ----
+	static const uint MATRIX_COUNT = 128;
 
-	uint Energy16_X16; // <free_16><range_8>
-	uint ColorPacked;
-	uint2 _Padding;
+	struct ShaderLight
+	{
+		float3 Position;
+		uint Type8_Flags8_Range16; // <Range_16><flags_8><type_8>
+
+		// -- 16 byte boundary ----
+		uint2 Direction16_ConeAngleCos16;
+		uint Energy16_X16; // <free_16><range_8>
+		uint ColorPacked;
+		uint Indices; // Not current packed
+
+		// -- 16 byte boundary ----
+		float4 ShadowAtlasMulAdd;
+
+		// -- 16 byte boundary ----
+		// Near first 16 bits, far being the lather
+		uint CubemapDepthRemapPacked;
 
 #ifndef __cplusplus
-	inline float4 GetColour()
-	{
-		float4 refVal;
+		inline float4 GetColour()
+		{
+			float4 refVal;
 
-		refVal.x = (float)((ColorPacked >> 0) & 0xFF) / 255.0f;
-		refVal.y = (float)((ColorPacked >> 8) & 0xFF) / 255.0f;
-		refVal.z = (float)((ColorPacked >> 16) & 0xFF) / 255.0f;
-		refVal.w = (float)((ColorPacked >> 24) & 0xFF) / 255.0f;
+			refVal.x = (float)((ColorPacked >> 0) & 0xFF) / 255.0f;
+			refVal.y = (float)((ColorPacked >> 8) & 0xFF) / 255.0f;
+			refVal.z = (float)((ColorPacked >> 16) & 0xFF) / 255.0f;
+			refVal.w = (float)((ColorPacked >> 24) & 0xFF) / 255.0f;
 
-		return refVal;
-	}
+			return refVal;
+		}
 
-	inline uint GetType()
-	{
-		return Type8_Flags8_Range16 & 0xFF;
-	}
+		inline float3 GetDirection()
+		{
+			return float3(
+				f16tof32(Direction16_ConeAngleCos16.x & 0xFFFF),
+				f16tof32((Direction16_ConeAngleCos16.x >> 16) & 0xFFFF),
+				f16tof32(Direction16_ConeAngleCos16.y & 0xFFFF)
+			);
+		}
 
-	inline uint GetFlags()
-	{
-		return (Type8_Flags8_Range16 >> 8) & 0xFF;
-	}
+		inline float GetConeAngleCos()
+		{
+			return f16tof32((Direction16_ConeAngleCos16.y >> 16) & 0xFFFF);
+		}
 
-	inline float GetRange()
-	{
-		return f16tof32((Type8_Flags8_Range16 >> 16) & 0xFFFF);
-	}
+		inline uint GetType()
+		{
+			return Type8_Flags8_Range16 & 0xFF;
+		}
 
-	inline float GetEnergy()
-	{
-		return f16tof32(Energy16_X16 & 0xFFFF);
-	}
+		inline uint GetFlags()
+		{
+			return (Type8_Flags8_Range16 >> 8) & 0xFF;
+		}
+
+		inline float GetRange()
+		{
+			return f16tof32((Type8_Flags8_Range16 >> 16) & 0xFFFF);
+		}
+
+		inline float GetEnergy()
+		{
+			return f16tof32(Energy16_X16 & 0xFFFF);
+		}
+
+		inline float GetCubemapDepthRemapNear(float value)
+		{
+			return f16tof32((CubemapDepthRemapPacked) & 0xFFFF);
+		}
+
+		inline float GetCubemapDepthRemapFar(float value)
+		{
+			return f16tof32((CubemapDepthRemapPacked >> 16u) & 0xFFFF);
+		}
 
 #else
-	inline void SetType(uint type)
-	{
-		Type8_Flags8_Range16 |= type & 0xFF;
-	}
+		inline void SetType(uint type)
+		{
+			Type8_Flags8_Range16 |= type & 0xFF;
+		}
 
-	inline void SetFlags(uint flags)
-	{
-		Type8_Flags8_Range16 |= (flags & 0xFF) << 8;
-	}
+		inline void SetFlags(uint flags)
+		{
+			Type8_Flags8_Range16 |= (flags & 0xFF) << 8;
+		}
 
-	inline void SetRange(float value)
-	{
-		Type8_Flags8_Range16 |= DirectX::PackedVector::XMConvertFloatToHalf(value) << 16;
-	}
+		inline void SetRange(float value)
+		{
+			Type8_Flags8_Range16 |= DirectX::PackedVector::XMConvertFloatToHalf(value) << 16;
+		}
 
-	inline void SetEnergy(float value)
-	{
-		Energy16_X16 |= DirectX::PackedVector::XMConvertFloatToHalf(value);
-	}
+		inline void SetEnergy(float value)
+		{
+			Energy16_X16 |= DirectX::PackedVector::XMConvertFloatToHalf(value);
+		}
+
+		inline void SetDirection(float3 value)
+		{
+			Direction16_ConeAngleCos16.x |= DirectX::PackedVector::XMConvertFloatToHalf(value.x);
+			Direction16_ConeAngleCos16.x |= DirectX::PackedVector::XMConvertFloatToHalf(value.y) << 16u;
+			Direction16_ConeAngleCos16.y |= DirectX::PackedVector::XMConvertFloatToHalf(value.z);
+		}
+
+		inline void SetConeAngleCos(float value)
+		{
+			Direction16_ConeAngleCos16.y |= DirectX::PackedVector::XMConvertFloatToHalf(value) << 16u;
+		}
+
+		inline void SetCubemapDepthRemapNear(float value)
+		{
+			CubemapDepthRemapPacked |= DirectX::PackedVector::XMConvertFloatToHalf(value);
+		}
+
+		inline void SetCubemapDepthRemapFar(float value)
+		{
+			CubemapDepthRemapPacked |= DirectX::PackedVector::XMConvertFloatToHalf(value) << 16u;
+		}
+
 #endif
-};
+	};
 
-struct ShaderTransform
-{
-	float4 Mat0;
-	float4 Mat1;
-	float4 Mat2;
+	struct ShaderTransform
+	{
+		float4 Mat0;
+		float4 Mat1;
+		float4 Mat2;
 
-	void init()
-	{
-		Mat0 = float4(1, 0, 0, 0);
-		Mat1 = float4(0, 1, 0, 0);
-		Mat2 = float4(0, 0, 1, 0);
-	}
-	void Create(float4x4 mat)
-	{
-		Mat0 = float4(mat._11, mat._21, mat._31, mat._41);
-		Mat1 = float4(mat._12, mat._22, mat._32, mat._42);
-		Mat2 = float4(mat._13, mat._23, mat._33, mat._43);
-	}
-	float4x4 GetMatrix()
+		void init()
+		{
+			Mat0 = float4(1, 0, 0, 0);
+			Mat1 = float4(0, 1, 0, 0);
+			Mat2 = float4(0, 0, 1, 0);
+		}
+		void Create(float4x4 mat)
+		{
+			Mat0 = float4(mat._11, mat._21, mat._31, mat._41);
+			Mat1 = float4(mat._12, mat._22, mat._32, mat._42);
+			Mat2 = float4(mat._13, mat._23, mat._33, mat._43);
+		}
+		float4x4 GetMatrix()
 #ifdef __cplusplus
-		const
+			const
 #endif // __cplusplus
+		{
+			return float4x4(
+				Mat0.x, Mat0.y, Mat0.z, Mat0.w,
+				Mat1.x, Mat1.y, Mat1.z, Mat1.w,
+				Mat2.x, Mat2.y, Mat2.z, Mat2.w,
+				0, 0, 0, 1
+			);
+		}
+	};
+
+	struct ImguiDrawInfo
 	{
-		return float4x4(
-			Mat0.x, Mat0.y, Mat0.z, Mat0.w,
-			Mat1.x, Mat1.y, Mat1.z, Mat1.w,
-			Mat2.x, Mat2.y, Mat2.z, Mat2.w,
-			0, 0, 0, 1
-		);
-	}
-};
+		float4x4 Mvp;
+		uint TextureIndex;
+	};
 
-struct ImguiDrawInfo
-{
-	float4x4 Mvp;
-	uint TextureIndex;
-};
+	struct FontVertex
+	{
+		float2 Position;
+		float2 TexCoord;
+	};
 
-struct FontVertex
-{
-	float2 Position;
-	float2 TexCoord;
-};
+	struct FontDrawInfo
+	{
+		float4x4 Mvp;
 
-struct FontDrawInfo
-{
-	float4x4 Mvp;
-
-	// -- 16 byte boundary ----
-	uint ColorPacked;
-	uint TextureIndex;
+		// -- 16 byte boundary ----
+		uint ColorPacked;
+		uint TextureIndex;
 
 #ifndef __cplusplus
-	inline float4 GetColour()
-	{
-		float4 refVal;
+		inline float4 GetColour()
+		{
+			float4 refVal;
 
-		refVal.x = (float)((ColorPacked >> 0) & 0xFF) / 255.0f;
-		refVal.y = (float)((ColorPacked >> 8) & 0xFF) / 255.0f;
-		refVal.z = (float)((ColorPacked >> 16) & 0xFF) / 255.0f;
-		refVal.w = (float)((ColorPacked >> 24) & 0xFF) / 255.0f;
+			refVal.x = (float)((ColorPacked >> 0) & 0xFF) / 255.0f;
+			refVal.y = (float)((ColorPacked >> 8) & 0xFF) / 255.0f;
+			refVal.z = (float)((ColorPacked >> 16) & 0xFF) / 255.0f;
+			refVal.w = (float)((ColorPacked >> 24) & 0xFF) / 255.0f;
 
-		return refVal;
-	}
+			return refVal;
+		}
 #endif
-};
+	};
 
-struct SceneData
-{
-	uint MeshBufferIndex;
-	uint GeometryBufferIndex;
-	uint MaterialBufferIndex;
-	uint IrradianceMapTexIndex;
+	struct SceneData
+	{
+		uint MeshBufferIndex;
+		uint GeometryBufferIndex;
+		uint MaterialBufferIndex;
+		uint IrradianceMapTexIndex;
 
-	// -- 16 byte boundary ----
+		// -- 16 byte boundary ----
 
-	uint PreFilteredEnvMapTexIndex;
-	uint NumLights;
-	uint _padding1;
-	uint _padding2;
+		uint PreFilteredEnvMapTexIndex;
+		uint NumLights;
+		uint _Padding0;
+		uint _Padding1;
 
-	// -- 16 byte boundary ----
-};
+		// -- 16 byte boundary ----
+	};
 
-struct MaterialData
-{
-	float3 AlbedoColour;
-	uint AlbedoTexture;
+	struct MaterialData
+	{
+		float3 AlbedoColour;
+		uint AlbedoTexture;
 
-	// -- 16 byte boundary ----
+		// -- 16 byte boundary ----
 
-	uint MaterialTexture;
-	uint RoughnessTexture;
-	uint MetalnessTexture;
-	uint AOTexture;
+		uint MaterialTexture;
+		uint RoughnessTexture;
+		uint MetalnessTexture;
+		uint AOTexture;
 
-	// -- 16 byte boundary ----
+		// -- 16 byte boundary ----
 
-	uint NormalTexture;
-	float Metalness;
-	float Roughness;
-	float AO;
+		uint NormalTexture;
+		float Metalness;
+		float Roughness;
+		float AO;
 
-	// -- 16 byte boundary ----
+		// -- 16 byte boundary ----
 
-	uint Flags;
-	uint EmissiveColourPacked;
-	uint2 _Padding;
+		uint Flags;
+		uint EmissiveColourPacked;
+		uint2 _Padding;
 
 #ifndef __cplusplus
-	inline float4 GetEmissiveColour()
-	{
-		float4 refVal;
+		inline float4 GetEmissiveColour()
+		{
+			float4 refVal;
 
-		refVal.x = (float)((EmissiveColourPacked >> 0) & 0xFF) / 255.0f;
-		refVal.y = (float)((EmissiveColourPacked >> 8) & 0xFF) / 255.0f;
-		refVal.z = (float)((EmissiveColourPacked >> 16) & 0xFF) / 255.0f;
-		refVal.w = (float)((EmissiveColourPacked >> 24) & 0xFF) / 255.0f;
+			refVal.x = (float)((EmissiveColourPacked >> 0) & 0xFF) / 255.0f;
+			refVal.y = (float)((EmissiveColourPacked >> 8) & 0xFF) / 255.0f;
+			refVal.z = (float)((EmissiveColourPacked >> 16) & 0xFF) / 255.0f;
+			refVal.w = (float)((EmissiveColourPacked >> 24) & 0xFF) / 255.0f;
 
-		return refVal;
-	}
+			return refVal;
+		}
 #endif
-};
+	};
 
-struct Mesh
-{
-	uint VbPositionBufferIndex;
-	uint VbTexCoordBufferIndex;
-	uint VbNormalBufferIndex;
-	uint VbTangentBufferIndex;
+	struct Mesh
+	{
+		uint VbPositionBufferIndex;
+		uint VbTexCoordBufferIndex;
+		uint VbNormalBufferIndex;
+		uint VbTangentBufferIndex;
 
-	// -- 16 byte boundary ----
-	uint Flags;
+		// -- 16 byte boundary ----
+		uint Flags;
 
-	uint _padding0;
-	uint _padding1;
-	uint _padding2;
+		uint _padding0;
+		uint _padding1;
+		uint _padding2;
 
-	// -- 16 byte boundary ----
-};
+		// -- 16 byte boundary ----
+	};
 
-struct Geometry
-{
-	uint MaterialIndex;
-	uint NumIndices;
-	uint NumVertices;
-	uint IndexOffset;
+	struct Geometry
+	{
+		uint MaterialIndex;
+		uint NumIndices;
+		uint NumVertices;
+		uint IndexOffset;
 
-	// -- 16 byte boundary ---
-	uint VertexBufferIndex;
-	uint PositionOffset;
-	uint TexCoordOffset;
-	uint NormalOffset;
+		// -- 16 byte boundary ---
+		uint VertexBufferIndex;
+		uint PositionOffset;
+		uint TexCoordOffset;
+		uint NormalOffset;
 
-	// -- 16 byte boundary ---
-	uint TangentOffset;
-	uint3 _padding;
+		// -- 16 byte boundary ---
+		uint TangentOffset;
+		uint3 _padding;
 
-	// -- 16 byte boundary ---
-};
+		// -- 16 byte boundary ---
+	};
 
-struct Frame
-{
-	uint BrdfLUTTexIndex;
-	uint _padding0;
-	uint _padding1;
-	uint _padding2;
+	struct Frame
+	{
+		uint BrdfLUTTexIndex;
+		uint LightEntityIndex;
+		uint MatricesIndex;
+		uint _padding2;
 
-	// -- 16 byte boundary ----
-	
-	SceneData Scene;
-};
+		// -- 16 byte boundary ----
 
-struct Camera
-{
-	float4x4 ViewProjection;
+		SceneData Scene;
+	};
 
-	// -- 16 byte boundary ----
+	struct Camera
+	{
+		float4x4 ViewProjection;
 
-	float4x4 ShadowViewProjection;
+		// -- 16 byte boundary ----
+		float4x4 ProjInv;
 
-	// -- 16 byte boundary ----
+		// -- 16 byte boundary ----
+		float4x4 ViewInv;
 
-	float3 CameraPosition;
-};
+		// -- 16 byte boundary ----
+
+		float4x4 ShadowViewProjection;
+
+		// -- 16 byte boundary ----
+
+		float3 CameraPosition;
+	};
 
 #define DRAW_FLAG_ALBEDO        0x001
 #define DRAW_FLAG_NORMAL        0x002
@@ -279,19 +343,26 @@ struct Camera
 #define DRAW_FLAG_EMISSIVE		0x080
 #define DRAW_FLAGS_DISABLE_IBL  0x100
 
-struct GeometryPassPushConstants
-{
-	float4x4 WorldTransform;
+	struct GeometryPassPushConstants
+	{
+		float4x4 WorldTransform;
 
-	uint GeometryIndex;
-	uint MeshIndex;
-	uint DrawFlags;
-};
+		uint GeometryIndex;
+		uint MeshIndex;
+		uint DrawFlags;
+	};
 
-struct ImagePassPushConstants
-{
+	struct ImagePassPushConstants
+	{
 
-};
+	};
+
+	struct DefferedLightingCSConstants
+	{
+		uint2 DipatchGridDim; // // Arguments of the Dispatch call
+		uint MaxTileWidth; // 8, 16 or 32.
+	};
+
 #ifdef __cplusplus
 }
 #endif
