1f3592d8453ef166a923bb36e446b19a7e21e5d6
Impulse21 <dipaolo21@gmail.com>
2022-09-26 14:43:47 -0400

    Fix shader

--- PhxEngine/Shaders/Sky_ProceduralPS.hlsl
+++ PhxEngine/Shaders/Sky_ProceduralPS.hlsl
@@ -9,13 +9,11 @@ struct PSInput
 [RootSignature(PHX_ENGINE_DEFAULT_ROOTSIGNATURE)]
 float4 main(PSInput input) : SV_TARGET
 {
-	float4 viewPos = mul(GetCamera().ProjInv, float4(input.ClipSpace, 0.0f, 1.0f));
-	viewPos /= viewPos.w;
+	float4 unprojected = mul(float4(input.ClipSpace, 0.0f, 1.0f), GetCamera().ViewProjectionInv);
+	unprojected.xyz /= unprojected.w;
 
-	float4 worldPos = mul(GetCamera().ViewInv, viewPos);
-	const float3 viewVector = normalize(worldPos.xyz - GetCamera().CameraPosition);
-
-	float3 skyColour = GetProceduralSkyColour(viewVector);
+	const float3 V = normalize(unprojected.xyz - GetCamera().CameraPosition);
+	float3 skyColour = GetProceduralSkyColour(V);
 
 	return float4(skyColour, 1.0f);
 
