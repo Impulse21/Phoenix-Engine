7fc045e73231fdb8fcf19d68a45524af41563117
Christopher DiPaolo <dipaolo21@gmail.com>
2023-03-11 13:13:45 -0500

    Working on memory Allocators - going to learn from Raptor Engine :)

---
+++ PhxEngine/Src/Core/Memory.cpp
@@ -0,0 +1,218 @@
+#include "C:/Users/dipao/source/repos/Impulse21/Phoenix-Engine/Build/PhxEngine/CMakeFiles/PhxEngine.dir/Debug/cmake_pch.hxx"
+
+#include <PhxEngine/Core/Memory.h>
+#include <tlsf.h>
+
+#define HEAP_ALLOCATOR_STATS
+
+using namespace PhxEngine;
+using namespace PhxEngine::Core;
+
+namespace
+{
+	void ExitWalker(void* ptr, size_t size, int used, void* user)
+	{
+		MemoryStatistics* stats = (MemoryStatistics*)user;
+		stats->add(used ? size : 0);
+
+	}
+
+	size_t Align(size_t size, size_t alignment)
+	{
+		const size_t alignmentMask = alignment - 1;
+		return (size + alignmentMask) & ~alignmentMask;
+	}
+}
+void PhxEngine::Core::HeapAllocator::Initialize(size_t size)
+{
+	this->m_memory = malloc(size);
+	this->m_maxSize = size;
+	this->m_allocatedSize = 0;
+	this->m_tlsfHandle = tlsf_create_with_pool(this->m_memory, size);
+}
+
+void PhxEngine::Core::HeapAllocator::Finalize()
+{
+	// Check Memory
+	MemoryStatistics stats = {
+		.AllocatedBytes = 0,
+		.TotalBytes = this->m_maxSize,
+		.AllocationCount = 0 };
+
+	pool_t pool = tlsf_get_pool(this->m_tlsfHandle);
+	tlsf_walk_pool(pool, ExitWalker, static_cast<void*>(&stats));
+
+	// TODO: Add logging
+	assert(stats.AllocatedBytes == 0);
+
+	tlsf_destroy(this->m_tlsfHandle);
+	free(this->m_memory);
+}
+
+void PhxEngine::Core::HeapAllocator::BuildIU()
+{
+	// TODO:
+}
+
+void* PhxEngine::Core::HeapAllocator::Allocate(size_t size, size_t alignment)
+{
+#ifdef HEAP_ALLOCATOR_STATS
+	void* allocatedMemory = alignment == 1 ? tlsf_malloc(this->m_tlsfHandle, size) : tlsf_memalign(this->m_tlsfHandle, alignment, size);
+	size_t actualSize= tlsf_block_size(allocatedMemory);
+	this->m_allocatedSize += actualSize;
+
+	return allocatedMemory;
+
+#else
+	return tlsf_malloc(this->m_tlsfHandle, size);
+#endif // HEAP_ALLOCATOR_STATS
+}
+
+void* PhxEngine::Core::HeapAllocator::Allocate(size_t size, size_t alignment, std::string file, int32_t lineNum)
+{
+	return this->Allocate(size, alignment);
+}
+
+void PhxEngine::Core::HeapAllocator::Deallocate(void* pointer)
+{
+#ifdef HEAP_ALLOCATOR_STATS
+	size_t actualSize = tlsf_block_size(pointer);
+	this->m_allocatedSize -= actualSize;
+
+	tlsf_free(this->m_tlsfHandle, pointer);
+#else
+	tlsf_free(this->m_tlsfHandle, pointer);
+#endif
+}
+
+void PhxEngine::Core::StackAllocator::Initialize(size_t size)
+{
+	this->m_memory = static_cast<uint8_t*>(malloc(size));
+	this->m_totalSize = size;
+	this->m_allocatedSize = 0;
+}
+
+void PhxEngine::Core::StackAllocator::Finalize()
+{
+	free(this->m_memory);
+}
+
+void* PhxEngine::Core::StackAllocator::Allocate(size_t size, size_t alignment)
+{
+	assert(size > 0);
+	const size_t newStart = Align(size, alignment);
+	assert(newStart < this->m_totalSize);
+
+	const size_t newAllocatedSize = newStart + size;
+	if (newAllocatedSize > this->m_totalSize)
+	{
+		assert(false);
+		// report overflow
+		return nullptr;
+	}
+
+	this->m_allocatedSize = newAllocatedSize;
+	return this->m_memory + newStart;
+}
+
+void* PhxEngine::Core::StackAllocator::Allocate(size_t size, size_t alignment, std::string file, int32_t lineNum)
+{
+	return this->Allocate(size, alignment);
+}
+
+void PhxEngine::Core::StackAllocator::Deallocate(void* pointer)
+{
+	assert(pointer > this->m_memory);
+	assert(pointer < this->m_memory + this->m_totalSize);
+	assert(pointer < this->m_memory + this->m_allocatedSize);
+
+	const size_t sizeAtPointer = static_cast<uint8_t*>(pointer) - this->m_memory;
+
+	this->m_allocatedSize = sizeAtPointer;
+}
+
+size_t PhxEngine::Core::StackAllocator::GetMarker()
+{
+	return this->m_allocatedSize;
+}
+
+void PhxEngine::Core::StackAllocator::FreeMarker(size_t marker)
+{
+	const size_t difference = marker - this->m_allocatedSize;
+	if (difference > 0)
+	{
+		this->m_allocatedSize = marker;
+	}
+}
+
+void PhxEngine::Core::StackAllocator::Clear()
+{
+	this->m_allocatedSize = 0;
+}
+
+void PhxEngine::Core::LinearAllocator::Initialize(size_t size)
+{
+	this->m_memory = static_cast<uint8_t*>(malloc(size));
+	this->m_totalSize = size;
+	this->m_allocatedSize = 0;
+}
+
+void PhxEngine::Core::LinearAllocator::Finalize()
+{
+	this->Clear();
+	free(this->m_memory);
+}
+
+void* PhxEngine::Core::LinearAllocator::Allocate(size_t size, size_t alignment)
+{
+	assert(size > 0);
+
+	const size_t newStart = Align(this->m_allocatedSize, alignment);
+	assert(newStart < this->m_totalSize);
+	const size_t newAllocatedSize = newStart + size;
+	if (newAllocatedSize > this->m_totalSize)
+	{
+		assert(false);
+		return nullptr;
+	}
+
+	this->m_allocatedSize = newAllocatedSize;
+
+	return this->m_memory + newStart;
+}
+
+void* PhxEngine::Core::LinearAllocator::Allocate(size_t size, size_t alignment, std::string file, int32_t lineNum)
+{
+	return this->Allocate(size, alignment);
+}
+
+void PhxEngine::Core::LinearAllocator::Deallocate(void* pointer)
+{
+	// NoOp
+}
+
+void PhxEngine::Core::LinearAllocator::Clear()
+{
+	this->m_allocatedSize = 0;
+}
+
+MemoryService& PhxEngine::Core::MemoryService::GetInstance()
+{
+	static MemoryService instance;
+	return instance;
+}
+
+void PhxEngine::Core::MemoryService::Initialize(PhxEngine::Core::MemoryServiceConfiguration const& config)
+{
+	this->m_systemAllocator.Initialize(config.MaximumDynamicSize);
+}
+
+void PhxEngine::Core::MemoryService::Finalize()
+{
+	this->m_systemAllocator.Finalize();
+}
+
+void PhxEngine::Core::MemoryService::BuildUI()
+{
+	// TODO:
+}
