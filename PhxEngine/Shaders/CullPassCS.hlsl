62700a6c644335f654b2b49c8b3ec4c30ff505f8
Christopher DiPaolo <dipaolo21@gmail.com>
2023-03-27 15:35:59 -0400

    Put back scale.

--- PhxEngine/Shaders/CullPassCS.hlsl
+++ PhxEngine/Shaders/CullPassCS.hlsl
@@ -60,8 +60,7 @@ void main(uint3 DTid : SV_DispatchThreadID, uint3 Gid : SV_GroupID, uint groupIn
         // Transform into view projection space
         float4 boundCentreWS = mul(float4(geometryBoundingSphere.xyz, 1.0f), objectInstance.WorldMatrix);
         float4 boundCentreVS = mul(boundCentreWS, GetCamera().ViewProjection);
-        // float radius = geometryBoundingSphere.w * 1.1;
-        float radius = geometryBoundingSphere.w;
+        float radius = geometryBoundingSphere.w * 1.1;
         
         bool isVisibleFrustum = true;
         for (uint i = 0; i < 6; i++)
