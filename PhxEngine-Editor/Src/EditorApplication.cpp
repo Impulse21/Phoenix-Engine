c801899f387eb6612d8e4b247960a34035f7801c
Christopher DiPaolo <dipaolo21@gmail.com>
2023-09-25 12:59:43 -0400

    Worked on thread dispatching

--- PhxEngine-Editor/Src/EditorApplication.cpp
+++ PhxEngine-Editor/Src/EditorApplication.cpp
@@ -3,6 +3,9 @@
 #include <PhxEngine/Core/CommandLineArgs.h>
 #include <PhxEngine/Engine/GameApplication.h>
 
+// -- Add to engine ---
+#include <PhxEngine/Core/WorkerThreadPool.h>
+
 #ifdef _MSC_VER // Windows
 #include <shellapi.h>
 #endif 
@@ -19,8 +22,11 @@ public:
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	// Engine will use one GB
-	Core::Log::Initialize();
+	// -- Engine Start-up ---
+	{
+		Core::Log::Initialize();
+		Core::WorkerThreadPool::Initialize();
+	}
 
 	// Test Allocating an object
 	Foo* foo = phx_new(Foo);
@@ -28,23 +34,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	assert(0 != SystemMemory::GetMemUsage());
 	phx_delete(foo);
 
+	const size_t memSize = PhxGB(1);
+	const size_t numElems = memSize / sizeof(Foo);
+	auto* data = phx_new_arr(Foo, numElems);
 
-	size_t newSize = PhxGB(1);
-	auto* data = phx_new_arr(Foo, newSize / sizeof(Foo));
-	auto* data1 = phx_new_arr(uint32_t, newSize / sizeof(uint32_t));
-	auto* data2 = phx_new_arr(uint32_t, newSize / sizeof(uint32_t));
+	for (int i = 0; i < numElems; i++)
+	{
 
-	size_t data0Size = phx_arr_len(data);
-	size_t data1Size = phx_arr_len(data1);
-	size_t data2Size = phx_arr_len(data2);
+	}
+	Core::WorkerThreadPool::Dispatch()
 
-	phx_delete_arr(data);
-	phx_delete_arr(data1);
-	phx_delete_arr(data2);
-
-	Core::ObjectTracker::Finalize();
-
-	assert(0 == SystemMemory::GetMemUsage());
-	SystemMemory::Cleanup();
-
+	// -- Finalize Block ---
+	{
+		Core::ObjectTracker::Finalize();
+		Core::WorkerThreadPool::Finalize();
+		assert(0 == SystemMemory::GetMemUsage());
+		SystemMemory::Cleanup();
+	}
 }
--- PhxEngine/Include/PhxEngine/Core/WorkerThreadPool.h
+++ PhxEngine/Include/PhxEngine/Core/WorkerThreadPool.h
@@ -7,13 +7,18 @@ namespace PhxEngine::Core
 	using TaskID = uint64_t;
 	namespace WorkerThreadPool
 	{
+		struct TaskArgs
+		{
 
-		void Initialize();
+		};
+
+		void Initialize(uint32_t maxThreadCount = ~0u);
 		void Finalize();
 
 		uint32_t GetThreadCount();
 
 		// void Run(context& ctx, const std::function<void(JobArgs)>& task);
+		TaskID Dispatch(uint32_t numTasks, uint32_t groupSize, std::function<void(TaskArgs)>& callback);
 	}
 }
 
