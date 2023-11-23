0dd5a49f06ca515b479c24b38dd7766c5578553f
Christopher DiPaolo <dipaolo21@gmail.com>
2023-11-22 21:12:59 -0500

    Trying out new API for graph

--- PhxEngine/Include/PhxEngine/Renderer/RenderGraph/RenderGraph.h
+++ PhxEngine/Include/PhxEngine/Renderer/RenderGraph/RenderGraph.h
@@ -16,6 +16,123 @@ namespace PhxEngine::RHI
 
 namespace PhxEngine::Renderer
 {
+	namespace RG
+	{
+		enum class ReferenceType : uint32_t
+		{
+			Invalid,
+			RenderTarget,
+			PassResult,
+			ExternalSRV
+
+		};
+
+		struct Reference
+		{
+			enum : uint8_t
+			{
+				AllSubresources = 0x7f
+			};
+
+			union
+			{
+				struct
+				{
+					ReferenceType Type : 2;
+					uint32_t Depth : 1;
+					uint32_t EntireTexture : 1;		 // Used for PassResult types to specify AllSubresources (since SubResource is used for the output index)
+					uint32_t MipSlice : 1;
+					uint32_t SubResource : 7;
+					uint32_t MipLevel : 4;
+					uint32_t Index : 16;
+				};
+				uint32_t Data;
+			};
+
+			Reference() {};
+
+			constexpr Reference(const ReferenceType type, const uint32_t index, const uint32_t sub_resource, const uint32_t depth = 0, const uint32_t entire_texture = 0, const uint32_t mip_slice = 0, const uint32_t mip_level = 0) 
+				: Type(type)
+				, Depth(depth)
+				, EntireTexture(entire_texture)
+				, MipSlice(mip_slice)
+				, SubResource(sub_resource)
+				, MipLevel(mip_level)
+				, Index(index)
+			{
+				assert(sub_resource < 128);
+				assert(index < 65536);
+				assert(mip_slice == 1 || mip_level == 0);
+				assert(mip_level < 16);
+			}
+
+			constexpr Reference SubResourceRef(const uint32_t sub_resource) const { return Reference(Type, Index, sub_resource, Depth); }
+			constexpr Reference MipSliceRef(const uint32_t mip_level) const { return Reference(Type, Index, 0, Depth, 0, 1, mip_level); }
+
+			static constexpr Reference Null()
+			{
+				return Reference(ReferenceType::Invalid, 0, 0);
+			}
+
+			explicit operator bool() const { return Data != 0; }
+		};
+
+		static_assert(sizeof(Reference) == sizeof(uint32_t));
+
+
+		struct RenderTarget
+		{
+			int Index = -1;
+
+			RenderTarget() {}
+			explicit RenderTarget(const int index) : Index(index) {}
+
+			constexpr operator Reference() const { return Reference(ReferenceType::RenderTarget, Index, 0); }
+			constexpr Reference SubResource(const uint32_t sub_resource) const { return Reference(ReferenceType::RenderTarget, Index, sub_resource); }
+			constexpr Reference MipSlice(const uint32_t mip_level) const { return Reference(ReferenceType::RenderTarget, Index, 0, 0, 0, 1, mip_level); }
+			constexpr Reference AllSubResources() const { return SubResource(Reference::AllSubresources); }
+		};
+
+		struct PassResult
+		{
+			int Index = -1;
+
+			PassResult() {}
+			explicit PassResult(const int index) : Index(index) {}
+
+			constexpr Reference Colour(const int index, const bool entire_texture = false) const { return Reference(ReferenceType::PassResult, Index, index, 0, entire_texture); }
+			constexpr Reference Depth(const bool entire_texture = false) const { return Reference(ReferenceType::PassResult, Index, 0, 1, entire_texture); }
+		};
+
+		class RenderGraphBuilder;
+		class Pass : RgNonCopyable
+		{
+			friend RenderGraphBuilder;
+		public:
+			Pass& Reads(Core::Span<Reference> references);
+
+			template<typename TExecuteLambda>
+			Pass& Execute(TExecuteLambda&& func)
+			{
+
+			}
+
+		private:
+		};
+
+		class RenderGraphBuilder
+		{
+		public:
+			RenderGraphBuilder(Core::IAllocator* allocoator);
+			RenderGraphBuilder(const RenderGraphBuilder&) = delete;
+
+			void Execute();
+
+			Pass& BeginPass();
+			PassResult EndPass(Pass& pass);
+		};
+	}
+
 	struct RgNonCopyable
 	{
 		RgNonCopyable() = default;
@@ -76,7 +193,7 @@ namespace PhxEngine::Renderer
 		RgResourceType	Type	: 15; // 14 bit to represent type, might remove some bits from this and give it to version
 		RgResourceFlags Flags	: 1;
 		uint64_t		Version : 16; // 16 bits to represent version should be more than enough, we can always just increase bit used if is not enough
-		uint64_t		Id		: 32; // 32 bit unsigned int
+		uint64_t		Id		: 32; // 32 bit uint32_t
 	};
 
 	// static_assert(sizeof(RgResourceHandle) == sizeof(uint64_t));
