5c0d1c10407b8f6255afe75f2f41a18265e9338e
Christopher DiPaolo <dipaolo21@gmail.com>
2024-03-28 09:38:33 -0400

    Setup overall frame structure

--- PhxEngine/Src/EngineLoop.cpp
+++ PhxEngine/Src/EngineLoop.cpp
@@ -14,6 +14,7 @@
 #include <PhxEngine/RHI/PhxRHI.h>
 
 #include <thread>
+#include <taskflow/taskflow.hpp>
 
 using namespace PhxEngine;
 
@@ -27,6 +28,7 @@ namespace
 	float m_deltaTimeAccumulator = 0.0f;
 
 	StopWatch m_gameClock = {};
+	tf::Executor m_executor;
 
 	void StartupEngineServices()
 	{
@@ -62,19 +64,19 @@ namespace
 		DisplaySubSystem::Ptr->Shutdown();
 	}
 
-	void FixedUpdate()
+	void FixedUpdate(IApplication* app, tf::Subflow& subflow)
 	{
 		PHX_EVENT();
 	}
 
-	void Update(TimeStep const span)
+	void Update(IApplication* app, TimeStep const span, tf::Subflow& subflow)
 	{
 		PHX_EVENT();
 		EngineMemory::Update();
 		RendererSubSystem::Ptr->Update();
 	}
 
-	void Render()
+	void Render(IApplication* app, tf::Subflow& subflow)
 	{
 		PHX_EVENT();
 
@@ -95,36 +97,53 @@ namespace
 				RHI::GpuBarrier::CreateTexture(gfxDevice->GetBackBuffer(), RHI::ResourceStates::RenderTarget, RHI::ResourceStates::Present)
 			},
 			composeCmdList);
-	}
-
-	void Tick()
-	{
-		DisplaySubSystem::Ptr->Update();
-		EventBus::DispatchEvents();
-
-		TimeStep deltaTimeSpan = m_gameClock.Elapsed();
-
-		// Fixed update calculation
-		{
-			m_deltaTimeAccumulator += deltaTimeSpan.GetSeconds();
-			if (m_deltaTimeAccumulator > 10)
-			{
-				// application probably lost control, fixed update would take too long
-				m_deltaTimeAccumulator = 0;
-			}
-
-			while (m_deltaTimeAccumulator >= m_targetFrameRateInv)
-			{
-				FixedUpdate();
-				m_deltaTimeAccumulator -= m_targetFrameRateInv;
-			}
-		}
-
-		Update(deltaTimeSpan);
-		Render();
 
 		// -- Present Screen ---
 		DisplaySubSystem::Ptr->Present();
+	}
+	void Tick(IApplication* app)
+	{
+		tf::Taskflow frameTaskflow;
+		TimeStep deltaTimeSpan = m_gameClock.Elapsed();
+
+		DisplaySubSystem::Ptr->Update();
+		EventBus::DispatchEvents();
+		deltaTimeSpan = m_gameClock.Elapsed();
+
+		tf::Task preRenderTask = frameTaskflow.emplace(
+			[&](tf::Subflow& subflow) {
+			}).name("Pre-Render");
+		
+
+		
+		tf::Task updateTask = frameTaskflow.emplace(
+			[&](tf::Subflow& subflow) {
+				m_deltaTimeAccumulator += deltaTimeSpan.GetSeconds();
+				if (m_deltaTimeAccumulator > 10)
+				{
+					// application probably lost control, fixed update would take too long
+					m_deltaTimeAccumulator = 0;
+				}
+
+				while (m_deltaTimeAccumulator >= m_targetFrameRateInv)
+				{
+					FixedUpdate(app, subflow);
+					m_deltaTimeAccumulator -= m_targetFrameRateInv;
+				}
+
+				Update(app, deltaTimeSpan, subflow);
+			}).name("Update");
+				
+		tf::Task renderTask = frameTaskflow.emplace(
+			[&](tf::Subflow& subflow) {
+					Render(app, subflow);
+				}).name("Render");
+
+
+		// Build Frame dependencies
+		preRenderTask.precede(updateTask, renderTask);
+
+		m_executor.run(frameTaskflow).wait();
 	}
 }
 
@@ -137,7 +156,7 @@ void PhxEngine::EngineLoop::Run(IApplication* app)
 	{
 		PHX_FRAME("MainThread");
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-		Tick();
+		Tick(app);
 	}
 
 	Shutdown(app);
