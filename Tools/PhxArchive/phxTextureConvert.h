6432abc846c9b879d43fd9fac598af9fa4de55c0
Impulse21 <dipaolo21@gmail.com>
2024-08-26 21:25:00 -0400

    hanges bit flags to my style

--- Tools/PhxArchive/phxTextureConvert.h
+++ Tools/PhxArchive/phxTextureConvert.h
@@ -5,13 +5,13 @@ namespace phx
 {
     enum TexConversionFlags
     {
-        kSRGB = 1,          // Texture contains sRGB colors
-        kPreserveAlpha = 2, // Keep four channels
-        kNormalMap = 4,     // Texture contains normals
-        kBumpToNormal = 8,  // Generate a normal map from a bump map
-        kDefaultBC = 16,    // Apply standard block compression (BC1-5)
-        kQualityBC = 32,    // Apply quality block compression (BC6H/7)
-        kFlipVertical = 64,
+        kSRGB = 1 << 0,          // Texture contains sRGB colors
+        kPreserveAlpha = 1 << 2, // Keep four channels
+        kNormalMap = 1 << 3,     // Texture contains normals
+        kBumpToNormal = 1 << 4,  // Generate a normal map from a bump map
+        kDefaultBC = 1 << 5,    // Apply standard block compression (BC1-5)
+        kQualityBC = 1 << 6,    // Apply quality block compression (BC6H/7)
+        kFlipVertical = 1 << 7,
     };
 
     inline uint8_t TextureOptions(bool sRGB, bool hasAlpha = false, bool invertY = false)
