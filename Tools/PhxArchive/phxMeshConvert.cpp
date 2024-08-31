d6824c1670ac84b25376f5082e00cabd2ffa90c6
Impulse21 <dipaolo21@gmail.com>
2024-08-31 11:17:23 -0400

    Mesh Compiler

--- Tools/PhxArchive/phxMeshConvert.cpp
+++ Tools/PhxArchive/phxMeshConvert.cpp
@@ -1,10 +1,33 @@
 #include "phxMeshConvert.h"
 
+#include <Core/phxLog.h>
 #include <cgltf/cgltf.h>
+#include <mesh-optimizer/meshoptimizer.h>
+
 using namespace phx;
 using namespace phx::MeshConverter;
 
-void phx::MeshConverter::OptimizeMesh(Primitive& outPrim, cgltf_primitive const& inPrim, DirectX::XMMATRIX const& localToObject)
+void phx::MeshConverter::OptimizeMesh(
+	Primitive& outPrim,
+	cgltf_primitive const& inPrim,
+	DirectX::XMMATRIX const& localToObject)
 {
 	// TODO: I AM HERE
+
+    if (inPrim.type != cgltf_primitive_type_triangles ||
+        inPrim.attributes_count == 0)
+    {
+        PHX_INFO("Found unsupported primitive topology");
+        return;
+    }
+
+	if (inPrim.indices == nullptr)
+	{
+		size_t indexCount = face_count * 3;
+		size_t unindexed_vertex_count = face_count * 3;
+		std::vector<unsigned int> remap(index_count); // allocate temporary memory for the remap table
+		size_t vertex_count = meshopt_generateVertexRemap(&remap[0], NULL, index_count, &unindexed_vertices[0], unindexed_vertex_count, sizeof(Vertex));
+
+
+	}
 }
