1295f7290d6bfe1f9d33f3c9203b919186f754f6
Impulse21 <dipaolo21@gmail.com>
2024-09-02 21:27:25 -0400

    Working on it.

--- Tools/PhxArchive/phxMeshConvert.cpp
+++ Tools/PhxArchive/phxMeshConvert.cpp
@@ -9,6 +9,18 @@
 using namespace phx;
 using namespace phx::MeshConverter;
 
+namespace
+{
+	std::pair<const uint8_t*, size_t> CgltfBufferAccessor(const cgltf_accessor* accessor, size_t defaultStride)
+	{
+		// TODO: sparse accessor support
+		const cgltf_buffer_view* view = accessor->buffer_view;
+		const uint8_t* Data = (uint8_t*)view->buffer->data + view->offset + accessor->offset;
+		const size_t stride = view->stride ? view->stride : defaultStride;
+		return std::make_pair(Data, stride);
+	}
+}
+
 void phx::MeshConverter::OptimizeMesh(
 	Primitive& outPrim,
 	cgltf_primitive const& inPrim,
@@ -44,6 +56,8 @@ void phx::MeshConverter::OptimizeMesh(
 		kNumAccessors,
 	};
 	std::array<const cgltf_accessor*, kNumAccessors> cgltfAccessors;
+	std::array<std::unique_ptr<uint8_t[]>, kNumAccessors> vertexData;
+	std::memset(cgltfAccessors.data(), 0, cgltfAccessors.size() * sizeof(cgltf_accessor*));
 
 	// Collect intreasted attributes
 	for (int iAttr = 0; iAttr < inPrim.attributes_count; iAttr++)
@@ -102,6 +116,28 @@ void phx::MeshConverter::OptimizeMesh(
 		}
 	}
 
+	if (cgltfAccessors[kPosition] == nullptr)
+	{
+		PHX_ERROR("Missing required Position attribute.");
+		return;
+	}
+
+	const size_t vertexCount = cgltfAccessors[kPosition]->count;
+	for (size_t i = 0; i < cgltfAccessors.size(); i++)
+	{
+		const cgltf_accessor* accessor = cgltfAccessors[i];
+		if (accessor)
+		{
+			return;
+		}
+
+		auto [data, dataStride] = CgltfBufferAccessor(accessor, accessor->stride * numComponents);
+		vertexData[i] = std::make_unique<uint8_t[]>(vertexCount * dataStride);
+		std::memcpy(
+			stream.get(),
+			data,
+			dataStride* accessor->count);
+	}
 
 	bool is32BitIndices = false;
 	if (inPrim.indices == nullptr)
@@ -118,5 +154,40 @@ void phx::MeshConverter::OptimizeMesh(
 
 		is32BitIndices = inPrim.indices->component_type == cgltf_component_type_r_32u;
 
+		const size_t indexCount = inPrim.indices->count;
+
+		auto [indexSrc, indexStride] = CgltfBufferAccessor(inPrim.indices, 0);
+		switch (inPrim.indices->stride)
+		{
+			case sizeof(uint8_t) :
+				for (size_t i = 0; i < indexCount; i += 3)
+				{
+					outMesh.Indices[indexOffset + i + 0] = vertexOffset + indexSrc[i + indexRemap[0]];
+					outMesh.Indices[indexOffset + i + 1] = vertexOffset + indexSrc[i + indexRemap[1]];
+					outMesh.Indices[indexOffset + i + 2] = vertexOffset + indexSrc[i + indexRemap[2]];
+				}
+			break;
+			case sizeof(uint16_t) :
+				for (size_t i = 0; i < indexCount; i += 3)
+				{
+					outMesh.Indices[indexOffset + i + 0] = vertexOffset + reinterpret_cast<const uint16_t*>(indexSrc)[i + indexRemap[0]];
+					outMesh.Indices[indexOffset + i + 1] = vertexOffset + reinterpret_cast<const uint16_t*>(indexSrc)[i + indexRemap[1]];
+					outMesh.Indices[indexOffset + i + 2] = vertexOffset + reinterpret_cast<const uint16_t*>(indexSrc)[i + indexRemap[2]];
+				}
+			break;
+			case sizeof(uint32_t) :
+				is32BitIndices = true;
+				for (size_t i = 0; i < indexCount; i += 3)
+				{
+					outMesh.Indices[indexOffset + i + 0] = vertexOffset + reinterpret_cast<const uint32_t*>(indexSrc)[i + indexRemap[0]];
+					outMesh.Indices[indexOffset + i + 1] = vertexOffset + reinterpret_cast<const uint32_t*>(indexSrc)[i + indexRemap[1]];
+					outMesh.Indices[indexOffset + i + 2] = vertexOffset + reinterpret_cast<const uint32_t*>(indexSrc)[i + indexRemap[2]];
+				}
+			break;
+			default:
+				assert(0 && "unsupported index stride!");
 	}
+
+
+	assert(material.index < 0x8000 && "Only 15-bit material indices allowed");
 }
