8d8870a1d62d565df259d4109e7674dd98a28f6b
Impulse21 <dipaolo21@gmail.com>
2024-09-13 19:27:38 -0400

    Deleting some old stuff

--- PhxEditor/AssetImporterGltf.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "AssetImporterGltf.h"
-
-bool phx::editor::AssetImporterGltf::ImporterInternal()
-{
-    return false;
-}
--- PhxEngine/Resource/phxResource.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-#include "phxResource.h"
-
-using namespace phx;
-
-
