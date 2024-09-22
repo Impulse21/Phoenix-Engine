dca2811987cbc09b6e6f4c800109f600a3eb2dc1
Christopher DiPaolo <dipaolo21@gmail.com>
2024-09-22 11:00:34 -0400

    Putting stuff back.

--- PhxEngine/EmberGfx/D3D12/phxGfxDeviceD3D12.cpp
+++ PhxEngine/EmberGfx/D3D12/phxGfxDeviceD3D12.cpp
@@ -5,6 +5,194 @@
 
 using namespace phx::gfx;
 
+
+namespace
+{
+	Microsoft::WRL::ComPtr<IDXGIFactory6> CreateDXGIFactory6(bool enableDebugLayers)
+	{
+		uint32_t flags = 0;
+		if (enableDebugLayers)
+		{
+			Microsoft::WRL::ComPtr<ID3D12Debug> debugController;
+			ThrowIfFailed(
+				D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
+
+			debugController->EnableDebugLayer();
+			flags = DXGI_CREATE_FACTORY_DEBUG;
+
+			Microsoft::WRL::ComPtr<ID3D12Debug3> debugController3;
+			if (SUCCEEDED(debugController->QueryInterface(IID_PPV_ARGS(&debugController3))))
+			{
+				debugController3->SetEnableGPUBasedValidation(false);
+			}
+
+			Microsoft::WRL::ComPtr<ID3D12Debug5> debugController5;
+			if (SUCCEEDED(debugController->QueryInterface(IID_PPV_ARGS(&debugController5))))
+			{
+				debugController5->SetEnableAutoName(true);
+			}
+		}
+
+		Microsoft::WRL::ComPtr<IDXGIFactory6> factory;
+		ThrowIfFailed(
+			CreateDXGIFactory2(flags, IID_PPV_ARGS(factory.ReleaseAndGetAddressOf())));
+
+		return factory;
+	}
+	bool SafeTestD3D12CreateDevice(IDXGIAdapter* adapter, D3D_FEATURE_LEVEL minFeatureLevel, D3D12DeviceBasicInfo& outInfo)
+	{
+#pragma warning(disable:6322)
+		try
+		{
+			ID3D12Device* Device = nullptr;
+			const HRESULT d3D12CreateDeviceResult = D3D12CreateDevice(adapter, minFeatureLevel, IID_PPV_ARGS(&Device));
+			if (SUCCEEDED(d3D12CreateDeviceResult))
+			{
+				outInfo.NumDeviceNodes = Device->GetNodeCount();
+
+				Device->Release();
+				return true;
+			}
+			else
+			{
+				PHX_CORE_WARN("D3D12CreateDevice failed.");
+			}
+		}
+		catch (...)
+		{
+		}
+#pragma warning(default:6322)
+
+		return false;
+	}
+
+	void FindAdapter(Microsoft::WRL::ComPtr<IDXGIFactory6> factory, D3D12Adapter& outAdapter)
+	{
+		PHX_CORE_INFO("Finding a suitable adapter");
+
+		// Create factory
+		Microsoft::WRL::ComPtr<IDXGIAdapter1> selectedAdapter;
+		D3D12DeviceBasicInfo selectedBasicDeviceInfo = {};
+		size_t selectedGPUVideoMemeory = 0;
+		Microsoft::WRL::ComPtr<IDXGIAdapter1> tempAdapter;
+		for (uint32_t adapterIndex = 0; D3D12Adapter::EnumAdapters(adapterIndex, factory.Get(), tempAdapter.ReleaseAndGetAddressOf()) != DXGI_ERROR_NOT_FOUND; ++adapterIndex)
+		{
+			if (!tempAdapter)
+			{
+				continue;
+			}
+
+			DXGI_ADAPTER_DESC1 desc = {};
+			tempAdapter->GetDesc1(&desc);
+
+			std::string name;
+			StringConvert(desc.Description, name);
+			size_t dedicatedVideoMemory = desc.DedicatedVideoMemory;
+			// size_t dedicatedSystemMemory = desc.DedicatedSystemMemory;
+			// size_t sharedSystemMemory = desc.SharedSystemMemory;
+
+			if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+			{
+				PHX_CORE_INFO("GPU '%s' is a software adapter. Skipping consideration as this is not supported.", name.c_str());
+				continue;
+			}
+
+			D3D12DeviceBasicInfo basicDeviceInfo = {};
+			if (!SafeTestD3D12CreateDevice(tempAdapter.Get(), D3D_FEATURE_LEVEL_11_1, basicDeviceInfo))
+			{
+				continue;
+			}
+
+			if (basicDeviceInfo.NumDeviceNodes > 1)
+			{
+				PHX_CORE_INFO("GPU '%s' has one or more device nodes. Currently only support one device ndoe.", name.c_str());
+			}
+
+			if (!selectedAdapter || selectedGPUVideoMemeory < dedicatedVideoMemory)
+			{
+				selectedAdapter = tempAdapter;
+				selectedGPUVideoMemeory = dedicatedVideoMemory;
+				selectedBasicDeviceInfo = basicDeviceInfo;
+			}
+		}
+
+		if (!selectedAdapter)
+		{
+			PHX_CORE_WARN("No suitable adapters were found.");
+			return;
+		}
+
+		DXGI_ADAPTER_DESC desc = {};
+		selectedAdapter->GetDesc(&desc);
+
+		std::string name;
+		StringConvert(desc.Description, name);
+		size_t dedicatedVideoMemory = desc.DedicatedVideoMemory;
+		size_t dedicatedSystemMemory = desc.DedicatedSystemMemory;
+		size_t sharedSystemMemory = desc.SharedSystemMemory;
+
+		// TODO: FIXLOG
+		PHX_CORE_INFO(
+			"Found Suitable D3D12 Adapter '%s'",
+			name.c_str());
+
+		// TODO: FIXLOG
+		PHX_CORE_INFO(
+			"Adapter has %dMB of dedicated video memory, %dMB of dedicated system memory, and %dMB of shared system memory.",
+			dedicatedVideoMemory / (1024 * 1024),
+			dedicatedSystemMemory / (1024 * 1024),
+			sharedSystemMemory / (1024 * 1024));
+
+		StringConvert(desc.Description, outAdapter.Name);
+		outAdapter.BasicDeviceInfo = selectedBasicDeviceInfo;
+		outAdapter.NativeDesc = desc;
+		outAdapter.NativeAdapter = selectedAdapter;
+	}
+
+}
+
+ID3D12CommandAllocator* D3D12CommandQueue::RequestAllocator()
+{
+	SCOPED_LOCK(this->MutexAllocation);
+
+	const uint64_t completedFenceValue = this->Fence->GetCompletedValue();
+	ID3D12CommandAllocator* retVal = nullptr;
+	if (!this->AvailableAllocators.empty())
+	{
+		auto& [fenceValue, allocator] = this->AvailableAllocators.front();
+		if (fenceValue < completedFenceValue)
+		{
+			retVal = allocator;
+			this->AvailableAllocators.pop_front();
+		}
+	}
+
+	if (!retVal)
+	{
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator>& newAllocator = this->AllocatorPool.emplace_back();
+		auto* device = D3D12Device::Impl();
+		dx::ThrowIfFailed(
+			device->GetD3D12Device2()->CreateCommandAllocator(this->Type, IID_PPV_ARGS(&newAllocator)));
+
+		newAllocator->SetName(L"Allocator");
+		retVal = newAllocator.Get();
+	}
+
+	return retVal;
+}
+
+phx::gfx::GfxDeviceD3D12::GfxDeviceD3D12()
+{
+	assert(Singleton == nullptr);
+	Singleton = this;
+}
+
+phx::gfx::GfxDeviceD3D12::~GfxDeviceD3D12()
+{
+	assert(Singleton);
+	Singleton = nullptr;
+}
+
 void phx::gfx::GfxDeviceD3D12::Initialize(SwapChainDesc const& swapChainDesc, void* windowHandle)
 {
 }
--- PhxEngine/EmberGfx/D3D12/phxGfxDeviceD3D12.h
+++ PhxEngine/EmberGfx/D3D12/phxGfxDeviceD3D12.h
@@ -2,19 +2,171 @@
 
 #include "EmberGfx/phxGfxDeviceInterface.h"
 
+#include <deque>
+#include <mutex>
+#define SCOPED_LOCK(x) std::scoped_lock _(x)
+
 namespace phx::gfx
 {
+
+	static const GUID RenderdocUUID = { 0xa7aa6116, 0x9c8d, 0x4bba, { 0x90, 0x83, 0xb4, 0xd8, 0x16, 0xb7, 0x1b, 0x78 } };
+	static const GUID PixUUID = { 0x9f251514, 0x9d4d, 0x4902, { 0x9d, 0x60, 0x18, 0x98, 0x8a, 0xb7, 0xd4, 0xb5 } };
+
+	struct D3D12DeviceBasicInfo final
+	{
+		uint32_t NumDeviceNodes;
+	};
+
+	enum class DescriptorHeapTypes : uint8_t
+	{
+		CBV_SRV_UAV,
+		Sampler,
+		RTV,
+		DSV,
+		Count,
+	};
+
+	struct D3D12Adapter final
+	{
+		std::string Name;
+		size_t DedicatedSystemMemory = 0;
+		size_t DedicatedVideoMemory = 0;
+		size_t SharedSystemMemory = 0;
+		D3D12DeviceBasicInfo BasicDeviceInfo;
+		DXGI_ADAPTER_DESC NativeDesc;
+		Microsoft::WRL::ComPtr<IDXGIAdapter1> NativeAdapter;
+
+		static HRESULT EnumAdapters(uint32_t adapterIndex, IDXGIFactory6* factory6, IDXGIAdapter1** outAdapter)
+		{
+			return factory6->EnumAdapterByGpuPreference(
+				adapterIndex,
+				DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
+				IID_PPV_ARGS(outAdapter));
+		}
+	};
+
+	struct D3D12SwapChain final
+	{
+		Microsoft::WRL::ComPtr<IDXGISwapChain1> SwapChain;
+		Microsoft::WRL::ComPtr<IDXGISwapChain4> SwapChain4;
+
+		std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, gfx::kBufferCount> BackBuffers;
+		DescriptorHeapAllocation Rtv;
+
+		bool Fullscreen : 1 = false;
+		bool VSync : 1 = false;
+		bool EnableHDR : 1 = false;
+	};
+
+
+	struct D3D12CommandQueue
+	{
+		D3D12_COMMAND_LIST_TYPE Type;
+		Microsoft::WRL::ComPtr<ID3D12CommandQueue> Queue;
+		Microsoft::WRL::ComPtr<ID3D12Fence> Fence;
+
+		std::vector<Microsoft::WRL::ComPtr<ID3D12CommandAllocator>> AllocatorPool;
+		std::deque<std::pair<uint64_t, ID3D12CommandAllocator*>> AvailableAllocators;
+
+		std::vector<ID3D12CommandList*> m_pendingSubmitCommands;
+		std::vector<ID3D12CommandAllocator*> m_pendingSubmitAllocators;
+
+		std::mutex MutexAllocation;
+		uint64_t NextFenceValue = 0;
+
+		void Initialize(ID3D12Device* nativeDevice, D3D12_COMMAND_LIST_TYPE type)
+		{
+			Type = type;
+
+			// Create Command Queue
+			D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+			queueDesc.Type = Type;
+			queueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+			queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+			queueDesc.NodeMask = 0;
+
+
+			dx::ThrowIfFailed(
+				nativeDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&Queue)));
+
+			// Create Fence
+			dx::ThrowIfFailed(
+				nativeDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&Fence)));
+			Fence->SetName(L"D3D12CommandQueue::D3D12CommandQueue::Fence");
+
+			this->NextFenceValue = this->Fence->GetCompletedValue() + 1;
+			switch (type)
+			{
+			case D3D12_COMMAND_LIST_TYPE_DIRECT:
+				Queue->SetName(L"Direct Command Queue");
+				break;
+			case D3D12_COMMAND_LIST_TYPE_COPY:
+				Queue->SetName(L"Copy Command Queue");
+				break;
+			case D3D12_COMMAND_LIST_TYPE_COMPUTE:
+				Queue->SetName(L"Compute Command Queue");
+				break;
+			}
+		}
+
+		uint64_t SubmitCommandLists(Span<ID3D12CommandList*> commandLists)
+		{
+			this->Queue->ExecuteCommandLists((UINT)commandLists.Size(), commandLists.begin());
+
+			return this->IncrementFence();
+		}
+
+		void DiscardAllocators(uint64_t fenceValue, Span<ID3D12CommandAllocator*> allocators)
+		{
+			SCOPED_LOCK(this->MutexAllocation);
+			for (ID3D12CommandAllocator* allocator : allocators)
+			{
+				this->AvailableAllocators.push_back(std::make_pair(fenceValue, allocator));
+			}
+		}
+
+		uint64_t IncrementFence()
+		{
+			this->Queue->Signal(this->Fence.Get(), this->NextFenceValue);
+			return this->NextFenceValue++;
+		}
+
+		uint64_t Submit()
+		{
+			const uint64_t fenceValue = this->SubmitCommandLists(this->m_pendingSubmitCommands);
+			this->m_pendingSubmitCommands.clear();
+
+			this->DiscardAllocators(fenceValue, this->m_pendingSubmitAllocators);
+			this->m_pendingSubmitAllocators.clear();
+
+			return fenceValue;
+		}
+
+		void EnqueueForSubmit(ID3D12CommandList* cmdList, ID3D12CommandAllocator* allocator)
+		{
+			this->m_pendingSubmitAllocators.push_back(allocator);
+			this->m_pendingSubmitCommands.push_back(cmdList);
+		}
+
+		ID3D12CommandAllocator* RequestAllocator();
+	};
+
 	class GfxDeviceD3D12 final : public IGfxDevice
 	{
 	public:
-		GfxDeviceD3D12() = default;
-		~GfxDeviceD3D12() = default;
+		inline static GfxDeviceD3D12* Instance() { return Singleton; }
+	public:
+		GfxDeviceD3D12();
+		~GfxDeviceD3D12();
 
 		void Initialize(SwapChainDesc const& swapChainDesc, void* windowHandle = nullptr) override;
 		void Finalize() override;
 
 		void WaitForIdle() override;
 		void ResizeSwapChain(SwapChainDesc const& swapChainDesc) override;
+
+	private:
+		inline static GfxDeviceD3D12* Singleton = nullptr;
 	};
 }
 
