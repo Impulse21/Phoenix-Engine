a98c90e48a061a5cd5b7fd7b1bc92bfbf4706b0a
Christopher DiPaolo <dipaolo21@gmail.com>
2024-12-05 12:04:12 -0500

    Working on thread pool

--- phxengine/include/phx/core/JobSystem.h
+++ phxengine/include/phx/core/JobSystem.h
@@ -1,25 +1,26 @@
 #pragma once
 
-// Ripped from Wicked engine: https://wickedengine.net/2018/11/simple-job-system-using-standard-c/comment-page-1/
 #include <functional>
 namespace phx
 {
-	struct JobDispatchArgs
+	namespace ThreadPool
 	{
-		uint32_t JobIndex = 0;
-		uint32_t GroupIndex = 0;
-	};
+		struct Barrier
+		{
+			std::atomic_int Counter;
+		};
 
-	namespace JobSystem
-	{
 		void Initialize();
-		void Execute(std::function<void()> const& job);
-
-		void Dispatch(uint32_t jobCount, uint32_t groupSize, std::function<void(JobDispatchArgs)> const& job);
+		void Finalize();
+		void Submit(std::function<void()> const& job);
 
 		bool IsBusy();
 
 		void Wait();
-	}
-}
 
+		void Wait(Barrier& barrier);
+
+		void Signal(Barrier& barrier);
+	}
+
+}
--- phxengine/src/core/JobSystem.cpp
+++ phxengine/src/core/JobSystem.cpp
@@ -1,33 +1,35 @@
-#include "pch.h"
+#include "ThreadPool.h"
 
-#include "phx/core/JobSystem.h"
-#include "phx/core/PlatformDetection.h"
-
-#include "phx/core/RingBuffer.h"
+#include "phxRingBuffer.h"
 #include <thread>
 #include <algorithm>
 #include <condition_variable>
 
-#ifdef PHX_PLATFORM_WINDOWS
+#ifdef _WIN32
+
 #define WIN32_LEAN_AND_MEAN
-#ifndef NOMINMAX
 #define NOMINMAX
-#endif
+
 #include <Windows.h>
 #include <sstream>
 #include <assert.h>
-#endif
 
+#endif
 using namespace phx;
 
 namespace
 {
+	using JobQueue = ThreadSafeRingBuffer<std::function<void()>, 256>;
+
 	uint32_t m_numThreads = 0;
-	ThreadSafeRingBuffer<std::function<void()>, 256> m_jobPool;
+	std::vector<std::thread> m_workerThreads;
+	std::unique_ptr<JobQueue[]> m_jobQueuePerThread;
 	std::condition_variable m_wakeCondition;
 	std::mutex m_wakeMutex;
 	uint64_t m_currentFenceValue;
 	std::atomic_uint64_t m_finishedFenceValue;
+	std::atomic_bool m_alive = false;
+	std::atomic_uint32_t m_nextQueue =  0 ;
 
 	void Poll()
 	{
@@ -35,67 +37,119 @@ namespace
 		std::this_thread::yield(); // allow this thread to be rescheduled;
 	}
 
+	// use R
+	struct Shutdowner
+	{
+		~Shutdowner()
+		{
+			ThreadPool::Finalize();
+		}
+	} m_shutdowner;
+
+
+	// Ran per thread - steals jobs.
+	void ThreadDoWork(size_t threadId)
+	{
+		std::function<void()> job;
+		for (size_t i = 0; i < m_numThreads; i++)
+		{
+			JobQueue& jobQueue = m_jobQueuePerThread[threadId % m_numThreads];
+			while (jobQueue.Pop(job))
+			{
+				job();
+			}
+			threadId++;
+		}
+	}
 }
 
-void JobSystem::Initialize()
+void ThreadPool::Initialize()
 {
 	m_finishedFenceValue.store(0ull);
 	m_currentFenceValue = 0;
 
-	uint32_t numCores = std::thread::hardware_concurrency();
+	const uint32_t numCores = std::thread::hardware_concurrency() - 1; // -1 for main thread.
 	m_numThreads = std::max(1u, numCores);
 
+	m_jobQueuePerThread.reset(new JobQueue[m_numThreads]);
 	for (uint32_t threadID = 0; threadID < m_numThreads; threadID++)
 	{
-		std::thread worker([]
-		{
-			std::function<void()> currentJob;
-
-			while(true)
+		std::thread& worker = m_workerThreads.emplace_back([threadID] {
+			while(m_alive)
 			{
-				if (m_jobPool.Pop(currentJob))
-				{
-					currentJob();
-					m_finishedFenceValue.fetch_add(1);
-				}
-				else
-				{
-					std::unique_lock<std::mutex> lock(m_wakeMutex);
-					m_wakeCondition.wait(lock);
-				}
+				ThreadDoWork(threadID);
+				
+				std::unique_lock<std::mutex> lock(m_wakeMutex);
+				m_wakeCondition.wait(lock);
 			}
 		});
 
 		// TODO:
-#ifdef PHX_PLATFORM_WINDOWS
+#ifdef _WIN32
 		HANDLE handle = (HANDLE)worker.native_handle();
 		DWORD_PTR affinityMask = 1ull << threadID;
 		DWORD_PTR affinityResult = SetThreadAffinityMask(handle, affinityMask);
 		assert(affinityResult > 0);
 
-		BOOL priorityResult = SetThreadPriority(handle, THREAD_PRIORITY_HIGHEST);
+		BOOL priorityResult = SetThreadPriority(handle, THREAD_PRIORITY_NORMAL);
 		assert(priorityResult != 0);
 
 		std::wstringstream wss;
-		wss << "phxJobSystem_" << threadID;
+		wss << "[PHX] ThreadPool_" << threadID;
 		HRESULT hr = SetThreadDescription(handle, wss.str().c_str());
 		assert(SUCCEEDED(hr));
 #endif
 		worker.detach();
 	}
+
+	m_alive.store(true);
 }
 
-void JobSystem::Execute(std::function<void()> const& job)
+void ThreadPool::Finalize()
+{
+	if (!m_alive)
+		return;
+
+	m_alive.store(false);
+	bool wakeLoop = true;
+
+	std::thread waker([&]
+	{
+		while(wakeLoop)
+		{
+			m_wakeCondition.notify_all();
+		}
+	});
+
+	for (auto& thread : m_workerThreads)
+		thread.join();
+
+	wakeLoop = false;
+	waker.join();
+
+	m_workerThreads.clear();
+	m_numThreads = 0;
+	m_jobQueuePerThread.reset();
+	m_nextQueue = 0;
+}
+
+void ThreadPool::Submit(std::function<void()> const& job)
 {
 	// Main Thread state
 	m_currentFenceValue += 1;
 
-	while (!m_jobPool.Push(job)) { Poll(); }
+	if ( m_numThreads == 1)
+	{
+		job();
+		return;
+	}
 
+	m_jobQueuePerThread[m_nextQueue.fetch_add(1) & m_numThreads].Push(job);
 	m_wakeCondition.notify_one();
 }
 
-void JobSystem::Dispatch(uint32_t jobCount, uint32_t groupSize, std::function<void(JobDispatchArgs)> const& job)
+#if false
+void ThreadPool::Dispatch(uint32_t jobCount, uint32_t groupSize, std::function<void(JobDispatchArgs)> const& job)
 {
 	if (jobCount == 0 || groupSize == 0)
 		return;
@@ -131,13 +185,35 @@ void JobSystem::Dispatch(uint32_t jobCount, uint32_t groupSize, std::function<vo
 		m_wakeCondition.notify_one(); // wake one thread
 	}
 }
+#endif
 
-bool JobSystem::IsBusy()
+bool ThreadPool::IsBusy()
 {
 	return m_finishedFenceValue.load() < m_currentFenceValue;
 }
 
-void JobSystem::Wait()
+void ThreadPool::Wait()
 {
 	while(IsBusy()) { Poll(); }
+}
+
+
+void ThreadPool::Wait(Barrier& barrier)
+{
+	barrier.Counter++;
+	
+	while (barrier.Counter.load() > 0)
+	{
+		m_wakeCondition.notify_all();
+		ThreadDoWork(m_nextQueue.fetch_add(1) % m_numThreads);
+
+		while (barrier.Counter.load() > 0)
+			std::this_thread::yield();
+	}
+}
+
+void ThreadPool::Signal(Barrier& barrier)
+{
+	barrier.Counter--;
+	m_wakeCondition.notify_one();
 }
