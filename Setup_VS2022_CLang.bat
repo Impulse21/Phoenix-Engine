178d4e753d457812dfc7f6e916ce91aac89580e8
Christopher DiPaolo <dipaolo21@gmail.com>
2024-12-18 23:56:48 -0500

    Alright Cmake script fixed up.

--- Setup_VS2022_CLang.bat
+++ Setup_VS2022_CLang.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist "Build" mkdir Build
-cd Build
+if not exist ".build" mkdir .build
+cd .build
 
-cmake -G "Visual Studio 17 2022" -A x64 ../
+cmake -G "Visual Studio 17 2022" -A x64 ../  -T ClangCL
 pause
