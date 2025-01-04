094629787e87eef877466b6d582b938999572ce4
Impulse21 <dipaolo21@gmail.com>
2025-01-04 13:35:32 -0500

    Adding Third part libaries

--- Setup_VS2022_CLang.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem if not exist ".build" mkdir .build
-rem cd .build
-
-rem cmake -G "Visual Studio 17 2022" -A x64 ../  -T ClangCL
-cmake --preset x64-Debug-Clang-Vs2022
-pause
--- VS2022_Dx12.bat
+++ VS2022_Dx12.bat
@@ -1,5 +1,5 @@
 @echo off
-start /B /W premake/premake5 --file=phxengine.lua vs2022
+start /B /W premake/premake5 --file=phxengine.lua vs2022 d3d12
 rem cd .vsfiles
 rem PhxEngine.sln
 echo Build finished succesfully
