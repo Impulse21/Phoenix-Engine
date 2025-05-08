bbcde3654ac92bf39f9d247f0b08fea93699d402
Christopher DiPaolo <dipaolo21@gmail.com>
2025-05-08 08:56:08 -0400

    Fixed Compile Issues.

--- PhxLibs/src/PhxData/Reflection.h
+++ PhxLibs/src/PhxData/Reflection.h
@@ -137,13 +137,13 @@ namespace phx::data
 		}
 
 		return TypeDescriptor{
-			.DefaultConstructor = defaultConstructor,
-			.Destructor = destructor,
-			.Name = std::string( name ),
+			.Name = std::string(name),
 			.Id = id,
 			.Size = sizeof(T),
 			.Base = base,
 			.Fields = std::vector(fields.begin(), fields.end()),
+			.DefaultConstructor = defaultConstructor,
+			.Destructor = destructor
 		};
 	}
 
@@ -238,7 +238,7 @@ namespace phx::HashUtils
 	void Combine(std::size_t& seed, const T& v, const Rest&... rest)
 	{
 		seed ^= std::hash<T>{}(v)+0x9e3779b9 + (seed << 6) + (seed >> 2);
-		(combine(seed, rest), ...);
+		(Combine(seed, rest), ...);
 	}
 }
 namespace std
