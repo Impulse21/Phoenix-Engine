c79d27f9ad521784b2ff2d4a87e4c89cf720f90f
Impulse21 <dipaolo21@gmail.com>
2025-05-15 21:12:19 -0400

    Working on mesh optimizer

--- PhxApps/PhxEditor/src/PhxEditorApp.cpp
+++ PhxApps/PhxEditor/src/PhxEditorApp.cpp
@@ -13,35 +13,147 @@ namespace
 {
 	void CompileObjAndMaterials(const char* filename, const char*)
 	{
-		fastObjMesh* objMesh = fast_obj_read(filename);
-		if (!objMesh)
+		phxed::MeshData meshSrc = {};
+		if (!ParseObj(filename, meshSrc))
+			return;
+
+		// Mesh Optimizer
+		const size_t totalIndices = meshSrc.Vertex_Positions.size();
+		std::array<meshopt_Stream, 3> vertexStream =
+		{
+			meshopt_Stream{
+				.data = meshSrc.Vertex_Positions.data(),
+				.size = meshSrc.Vertex_Positions.size(),
+				.stride = sizeof(meshSrc.Vertex_Positions[0])
+			},
+			meshopt_Stream{
+				.data = meshSrc.Vertex_Normals.data(),
+				.size = meshSrc.Vertex_Normals.size(),
+				.stride = sizeof(meshSrc.Vertex_Normals[0])
+			},
+			meshopt_Stream{
+				.data = meshSrc.Vertex_Uvset_0.data(),
+				.size = meshSrc.Vertex_Uvset_0.size(),
+				.stride = sizeof(meshSrc.Vertex_Uvset_0[0])
+			},
+		};
+
+		std::vector<uint32_t> remap(totalIndices);
+		size_t totalVertices =
+			meshopt_generateVertexRemapMulti(
+				&remap[0],
+				NULL,
+				totalIndices,
+				totalIndices,
+				vertexStream.data(),
+				vertexStream.size());
+
+
+		phxed::MeshData processedMesh = {};
+
+		processedMesh.Indices.resize(totalIndices);
+		meshopt_remapIndexBuffer(processedMesh.Indices.data(), NULL, totalIndices, remap.data());
+
+		processedMesh.Vertex_Positions.resize(totalVertices);
+		meshopt_remapVertexBuffer(
+			processedMesh.Vertex_Positions.data(),
+			meshSrc.Vertex_Positions.data(),
+			totalIndices,
+			sizeof(processedMesh.Vertex_Positions[0]),
+			&remap[0]);
+	
+		processedMesh.Vertex_Normals.resize(totalVertices);
+		meshopt_remapVertexBuffer(
+			processedMesh.Vertex_Normals.data(),
+			meshSrc.Vertex_Normals.data(),
+			totalIndices,
+			sizeof(processedMesh.Vertex_Normals[0]),
+			&remap[0]);
+
+		processedMesh.Vertex_Uvset_0.resize(totalVertices);
+		meshopt_remapVertexBuffer(
+			processedMesh.Vertex_Uvset_0.data(),
+			meshSrc.Vertex_Uvset_0.data(),
+			totalIndices,
+			sizeof(processedMesh.Vertex_Uvset_0[0]),
+			&remap[0]);
+
+
+	}
+
+	bool ParseObj(const char* filename, phxed::MeshData& meshData)
+	{
+		fastObjMesh* obj = fast_obj_read(filename);
+		if (!obj)
 		{
 			PHX_ERROR("Failed to Load. \n\tError {0}\n\tWarn {1}");
-			return;
+			return false;
 		}
 
-		phxed::MeshData meshData = {};
+		size_t totalIndices = 0;
 
-		uint32_t offset = 0;
-		for (uint32_t iFace = 0; iFace < objMesh->face_count; iFace++)
+		for (uint32_t i = 0; i < obj->face_count; ++i)
+			totalIndices += 3 * (obj->face_vertices[i] - 2);
+
+		meshData.Vertex_Positions.reserve(totalIndices);
+		meshData.Vertex_Normals.reserve(totalIndices);
+		meshData.Vertex_Tangents.reserve(totalIndices);
+		meshData.Vertex_Uvset_0.reserve(totalIndices);
+		meshData.Vertex_Uvset_1.reserve(totalIndices);
+
+		size_t vertexOffset = 0;
+		size_t indexOffset = 0;
+
+		for (size_t iFace = 0; iFace < obj->face_count; ++iFace)
 		{
-			uint32_t faceVertices = objMesh->face_vertices[iFace];
-
-			PHX_ASSERT(faceVertices == 3);
-
-			uint32_t idx0 = offset + 0;
-			uint32_t idx1 = offset + iFace + 1;
-			uint32_t idx2 = offset + iFace + 2;
-
-			for (uint32_t idx : { idx0, idx1, idx2 })
+			for (size_t iVert = 0; iVert < obj->face_vertices[iFace]; ++iVert)
 			{
-				const auto& v = objMesh->indices[idx];
-				posIndex.push_back(v.p);
-				normIndex.push_back(v.n);
-				uvIndex.push_back(v.t);
+				fastObjIndex gi = obj->indices[indexOffset + iVert];
+
+
+
+				// triangulate polygon on the fly; offset-3 is always the first polygon vertex
+				if (iVert >= 3)
+				{
+					meshData.Vertex_Positions[vertexOffset + 0] = meshData.Vertex_Positions[vertexOffset - 3];
+					meshData.Vertex_Normals[vertexOffset + 0] = meshData.Vertex_Normals[vertexOffset - 3];
+					meshData.Vertex_Uvset_0[vertexOffset + 0] = meshData.Vertex_Uvset_0[vertexOffset - 3];
+
+					meshData.Vertex_Positions[vertexOffset + 1] = meshData.Vertex_Positions[vertexOffset - 1];
+					meshData.Vertex_Normals[vertexOffset + 1] = meshData.Vertex_Normals[vertexOffset - 1];
+					meshData.Vertex_Uvset_0[vertexOffset + 1] = meshData.Vertex_Uvset_0[vertexOffset - 1];
+
+					vertexOffset += 2;
+				}
+
+				meshData.Vertex_Positions[vertexOffset] =
+				{
+					obj->positions[gi.p * 3 + 0],
+					obj->positions[gi.p * 3 + 1],
+					obj->positions[gi.p * 3 + 2],
+				};
+
+				meshData.Vertex_Normals[vertexOffset] =
+				{
+					obj->normals[gi.n * 3 + 0],
+					obj->normals[gi.n * 3 + 1],
+					obj->normals[gi.n * 3 + 2],
+				};
+
+				meshData.Vertex_Uvset_0[vertexOffset] =
+				{
+					obj->texcoords[gi.t * 2 + 0],
+					obj->texcoords[gi.t * 2 + 1],
+				};
+				vertexOffset++;
 			}
+
+			indexOffset += obj->face_vertices[iFace];
 		}
-		fast_obj_destroy(objMesh);
+
+		fast_obj_destroy(obj);
+
+		return true;
 	}
 }
 
