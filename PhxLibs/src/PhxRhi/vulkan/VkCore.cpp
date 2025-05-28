4e0353dd61af08134ef01e62aec6d64287fd215f
Christopher DiPaolo <dipaolo21@gmail.com>
2025-05-28 11:05:59 -0400

    Added optional extensions. VMA issue.

--- PhxLibs/src/PhxRhi/vulkan/VkCore.cpp
+++ PhxLibs/src/PhxRhi/vulkan/VkCore.cpp
@@ -14,6 +14,8 @@
 
 #include "VkCore.h"
 
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 #define VMA_IMPLEMENTATION
 #include "vk_mem_alloc.h"
 
@@ -116,7 +118,7 @@ namespace phx::rhi
 		feats.sparseBinding = true;
 		selector.set_required_features(feats);
 
-		const std::vector<const char*> extensions = 
+		const std::vector<const char*> required_extensions = 
 		{
 			VK_EXT_SAMPLER_FILTER_MINMAX_EXTENSION_NAME,
 			VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME,
@@ -125,26 +127,35 @@ namespace phx::rhi
 			VK_KHR_MULTIVIEW_EXTENSION_NAME,
 			VK_KHR_FRAGMENT_SHADING_RATE_EXTENSION_NAME,
 			VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME,
-			VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
 			VK_EXT_DESCRIPTOR_BUFFER_EXTENSION_NAME
 		};
 
-		auto device_retval = selector
+		vkb::PhysicalDevice physicalDevice = selector
 			.set_minimum_version(1, 3)
 			.prefer_gpu_device_type(vkb::PreferredDeviceType::discrete)
 			.require_separate_compute_queue()
 			.require_separate_transfer_queue()
 			.set_surface(g_VkContext.Surface)
-			.add_required_extensions(extensions.size(), extensions.data())
-			.select();
+			.add_required_extensions(required_extensions.size(), required_extensions.data())
+			.select()
+			.value();
 
-		if (device_retval.vk_result() != VK_SUCCESS)
+		const std::vector<const char*> optional_extensions =
 		{
-			PHX_CORE_ERROR("Failed to find a suitable device");
-			return;
-		}
+			VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
+			VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
+			VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
+			VK_EXT_DEVICE_GENERATED_COMMANDS_EXTENSION_NAME,
+			VK_EXT_GRAPHICS_PIPELINE_LIBRARY_EXTENSION_NAME,
+			// VK_EXT_SHADER_OBJECT_EXTENSION_NAME,
+			VK_KHR_PIPELINE_LIBRARY_EXTENSION_NAME,
+			VK_KHR_FRAGMENT_SHADING_RATE_EXTENSION_NAME,
+			VK_EXT_CONDITIONAL_RENDERING_EXTENSION_NAME,
+			VK_EXT_MESH_SHADER_EXTENSION_NAME,
+		};
 
-		vkb::PhysicalDevice physicalDevice = device_retval.value();
+		physicalDevice.enable_extensions_if_present(optional_extensions);
+
 		vkb::DeviceBuilder deviceBuilder{ physicalDevice };
 
 		vkb::Device vkbDevice = deviceBuilder.build().value();
@@ -152,6 +163,7 @@ namespace phx::rhi
 		// Get the VkDevice handle used in the rest of a vulkan application
 		g_VkContext.Device = vkbDevice.device;
 		g_VkContext.ChoosenPhysicalDevice = physicalDevice.physical_device;
+		g_VkContext.PhysicalDeviceProperties = physicalDevice.properties;
 
 		// use vkbootstrap to get a Graphics queue
 		g_VkContext.GfxQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
@@ -167,13 +179,43 @@ namespace phx::rhi
 		allocatorInfo.physicalDevice = g_VkContext.ChoosenPhysicalDevice;
 		allocatorInfo.device = g_VkContext.Device;
 		allocatorInfo.instance = g_VkContext.Instance;
-		vmaCreateAllocator(&allocatorInfo, &g_VkContext.VmaAllocator);
 
+		// Core in 1.1
+		allocatorInfo.flags =
+			VMA_ALLOCATOR_CREATE_KHR_DEDICATED_ALLOCATION_BIT |
+			VMA_ALLOCATOR_CREATE_KHR_BIND_MEMORY2_BIT;
 
-		vkGetPhysicalDeviceProperties(g_VkContext.ChoosenPhysicalDevice, &g_VkContext.PhysicalDeviceProperties);
+	
+		if (vkbDevice.physical_device.is_extension_present(VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME))
+		{
+			allocatorInfo.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT;
+		}
 
-		VkPhysicalDeviceMemoryProperties memProperties;
-		vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+#if VMA_DYNAMIC_VULKAN_FUNCTIONS
+		static VmaVulkanFunctions vulkanFunctions = {};
+		vulkanFunctions.vkGetInstanceProcAddr = vkGetInstanceProcAddr;
+		vulkanFunctions.vkGetDeviceProcAddr = vkGetDeviceProcAddr;
+		allocatorInfo.pVulkanFunctions = &vulkanFunctions;
+#endif
+
+		std::vector<VkExternalMemoryHandleTypeFlags> externalMemoryHandleTypes;
+#if defined(VK_USE_PLATFORM_WIN32_KHR)
+		externalMemoryHandleTypes.resize(physicalDevice.memory_properties.memoryTypeCount, VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_WIN32_BIT);
+		allocatorInfo.pTypeExternalMemoryHandleTypes = externalMemoryHandleTypes.data();
+#elif defined(__linux__)
+		externalMemoryHandleTypes.resize(physicalDevice.memory_properties.memoryTypeCount, VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT);
+		allocatorInfo.pTypeExternalMemoryHandleTypes = externalMemoryHandleTypes.data();
+#endif
+
+		VkResult res = vmaCreateAllocator(&allocatorInfo, &g_VkContext.VmaAllocator);
+		PHX_CORE_ASSERT(res == VK_SUCCESS);
+		if (res != VK_SUCCESS)
+		{
+			PHX_CORE_ERROR("Failed to create VMA allocator");
+			throw std::runtime_error("vmaCreateAllocator failed!");
+		}
+
+		VkPhysicalDeviceMemoryProperties memProperties = physicalDevice.memory_properties;
 
 		std::stringstream memory_ss;
 		for (uint32_t i = 0; i < memProperties.memoryHeapCount; ++i) 
