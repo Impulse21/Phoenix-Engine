b04f521c4210ab4c1194764b4887f47c3608e224
Christopher DiPaolo <dipaolo21@gmail.com>
2025-05-30 10:15:30 -0400

    Added back align.

--- PhxLibs/src/PhxCore/Memory.h
+++ PhxLibs/src/PhxCore/Memory.h
@@ -6,6 +6,12 @@
 
 namespace phx
 {
+	template<typename T, typename U>
+	constexpr T AlignUp(T Size, U Alignment)
+	{
+		return (T)(((size_t)Size + (size_t)Alignment - 1) & ~((size_t)Alignment - 1));
+	}
+	
 	struct MemoryStatistics
 	{
 		size_t AllocatedBytes = 0;
