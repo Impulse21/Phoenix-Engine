3b2d56bbd4692e5fea719d69a030e52ae4643cb3
Christopher DiPaolo <dipaolo21@gmail.com>
2025-06-13 11:04:35 -0400

    Reworked memory region.

--- PhxLibs/src/PhxCore/IO/MemoryRegion.h
+++ PhxLibs/src/PhxCore/IO/MemoryRegion.h
@@ -4,46 +4,69 @@
 
 namespace phx
 {
+	using byte = std::byte;
 
 	template<typename T>
+	class MemoryRegionView
+	{
+	public:
+		MemoryRegionView() = default;
+
+		explicit MemoryRegionView(T* ptr)
+			: m_ptr(ptr)
+		{
+		}
+
+		T* Get()
+		{
+			return m_ptr;
+		}
+
+		const T* Get() const
+		{
+			return m_ptr;
+		}
+
+		T* operator->()
+		{
+			return m_ptr;
+		}
+
+		T const* operator->() const
+		{
+			return m_ptr;
+		}
+
+	private:
+		T* m_ptr = nullptr;
+	};
+
 	class MemoryRegion
 	{
 	public:
 		MemoryRegion() = default;
 
-		MemoryRegion(std::unique_ptr<char[]> buffer)
+		explicit MemoryRegion(std::unique_ptr<byte[]> buffer, size_t size)
 			: m_buffer(std::move(buffer))
+			, m_size(size)
 		{
 		}
 
-		char* Data()
+		byte* Data()
 		{
 			return m_buffer.get();
 		}
 
-		T* Get()
-		{
-			return reinterpret_cast<T*>(m_buffer.get());
-		}
+		size_t Size() const { return m_size; }
 
-		const T* Get() const
+		template<typename T>
+		MemoryRegionView<T> GetView(size_t offset = 0)
 		{
-			return reinterpret_cast<T*>(m_buffer.get());
-		}
-
-		T* operator->()
-		{
-			return reinterpret_cast<T*>(m_buffer.get());
-		}
-
-		T const* operator->() const
-		{
-			return reinterpret_cast<T const*>(m_buffer.get());
+			return MemoryRegionView(reinterpret_cast<T*>(m_buffer.get() + offset));
 		}
 
 	private:
-		std::unique_ptr<char[]> m_buffer;
-
+		std::unique_ptr<byte[]> m_buffer;
+		size_t m_size = 0ull;
 	};
-
 }
