3d799dbbe00c0dc51e3335da9b10c4a7a0a4336f
Christopher DiPaolo <dipaolo21@gmail.com>
2025-06-13 12:06:43 -0400

    Added Replacement.

--- PhxLibs/src/PhxCore/IO/MemoryRegion.h
+++ PhxLibs/src/PhxCore/IO/MemoryRegion.h
@@ -4,6 +4,72 @@
 
 namespace phx
 {
+	using byte = std::byte;
+
+	template<typename T>
+	class TypedView
+	{
+	public:
+		TypedView() = default;
+
+		explicit TypedView(T* ptr)
+			: m_ptr(ptr)
+		{
+		}
+
+		T* Get()
+		{
+			return m_ptr;
+		}
+
+		const T* Get() const
+		{
+			return m_ptr;
+		}
+
+		T* operator->()
+		{
+			return m_ptr;
+		}
+
+		T const* operator->() const
+		{
+			return m_ptr;
+		}
+
+	private:
+		T* m_ptr = nullptr;
+	};
+
+	class MemoryBuffer
+	{
+	public:
+		MemoryBuffer() = default;
+
+		explicit MemoryBuffer(std::unique_ptr<byte[]> buffer, size_t size)
+			: m_buffer(std::move(buffer))
+			, m_size(size)
+		{
+		}
+
+		byte* Data()
+		{
+			return m_buffer.get();
+		}
+
+		size_t Size() const { return m_size; }
+
+		template<typename T>
+		TypedView<T> GetView(size_t offset = 0)
+		{
+			return TypedView(reinterpret_cast<T*>(m_buffer.get() + offset));
+		}
+
+	private:
+		std::unique_ptr<byte[]> m_buffer;
+		size_t m_size = 0ull;
+	};
+
 	// DEPERICATED
 	template<typename T>
 	class MemoryRegion
