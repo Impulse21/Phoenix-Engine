684d9e2b5f73bcab8b3516ecb316e23e2fb354f1
Christopher DiPaolo <dipaolo21@gmail.com>
2025-06-19 20:36:45 -0400

    Working on Upload context.

--- PhxLibs/src/PhxRhi/BaseCommandCtx.h
+++ PhxLibs/src/PhxRhi/BaseCommandCtx.h
@@ -1,24 +1,45 @@
 #pragma once
 
+#include <PhxRhi/RHICommon.h>
+
 namespace phx::rhi
 {
-	template<class TDerivedDevice>
-	class BaseCommandBuffer
+	template<class TDerivedCtx>
+	class BaseCommnadCtx
 	{
 	public:
 
+		void* GetStagingMemory(uint64_t size, uint64_t alignment = 1)
+		{
+			return static_cast<TDerivedCtx*>(this)->PlatformGetStagingMemory(size, alignment);
+		}
+
+		void CopyBuffer(rhi::GpuBufferHandle handle)
+		{
+			static_cast<TDerivedCtx*>(this)->PlatformCopyBuffer(handle);
+		}
+
+		void CopyTexture(rhi::TextureHandle handle)
+		{
+			static_cast<TDerivedCtx*>(this)->PlatformCopyTexture(handle);
+		}
 #if false
 		// Access to the native underlying command buffer object
 		auto GetPlatformHandle()
 			-> decltype(static_cast<TDerivedDevice*>(this)
 				->PlatformGetNativeHandle())
 		{
-			return static_cast<TDerivedDevice*>(this)->PlatformGetNativeHandle();
+			return static_cast<TDerivedCtx*>(this)->PlatformGetNativeHandle();
 		}
 #endif
 	protected:
+		BaseCommnadCtx() = default;
+		virtual ~BaseCommnadCtx() = default;
+	};
 
-		BaseCommandBuffer() = default;
-		~BaseCommandBuffer() = default;
+	template<class TDerivedCtx>
+	class BaseCopyCommnadCtx : public BaseCommnadCtx<TDerivedCtx>
+	{
+	public:
 	};
 }
