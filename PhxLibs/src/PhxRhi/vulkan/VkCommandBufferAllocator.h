a0eab597219100c829e1195c9de242be22b776cd
Christopher DiPaolo <dipaolo21@gmail.com>
2025-06-27 23:48:07 -0400

    Command Buffer management is hell.

--- PhxLibs/src/PhxRhi/vulkan/VkCommandBufferAllocator.h
+++ PhxLibs/src/PhxRhi/vulkan/VkCommandBufferAllocator.h
@@ -25,9 +25,10 @@ namespace phx::RHI::vk
 	private:
 		struct FrameCommandPool
 		{
+			std::mutex mutex;
 			VkCommandPool vk_cmd_pool = VK_NULL_HANDLE;
 			std::vector<VkCommandBuffer> buffers;
-			std::deque<size_t> m_free_indices;
+			size_t num_active_buffers;
 
 		};
 
@@ -48,24 +49,39 @@ namespace phx::RHI::vk
 		VkDevice m_vk_logical_device;
 		std::deque<size_t> m_free_indices;
 
-		std::mutex m_async_mutex;;
+		std::mutex m_async_mutex;
 		std::vector<CommandBuffer_VK> m_async_command_buffers;
 
-		std::mutex m_frame_mutex;
-		size_t m_num_frame_buffers;
-		std::vector<CommandBuffer_VK> m_frame_command_buffers;
-
-		FrameCommandPool m_frame_pools[NumCommandQueues];
-		AsyncCommandPool m_async_pools[NumCommandQueues];
-
+		std::mutex m_frame_registry_lock;
+		std::vector<std::unique_ptr<CommandBuffer_VK>> m_frame_buffer_registry;
+		EnumArray<FrameCommandPool,  CommandQueueType> m_frame_pools[_MaxFramesInFlight];
 		VkFence m_frame_fences[_MaxFramesInFlight];
+
+		AsyncCommandPool m_async_pools[NumCommandQueues];
 	};
 
 	template<size_t _MaxFramesInFlight>
 	inline CommandBufferHandle VkCommandBufferAllocator<_MaxFramesInFlight>::AcquireFrameBuffer(uint64_t frame_index, CommandQueueType queue_type)
 	{
-		std::scoped_lock _(m_frame_mutex);
-		size_t index = m_num_frame_buffers++;
+		FrameCommandPool& frame_command_pool = m_frame_fences[frame_index % _MaxFramesInFlight][queue_type];
+
+		CommandBufferHandle ret_val = {};
+		ret_val.data.pool_type = PoolType::Frame;
+		ret_val.data.queue_type = queue_type;
+
+		{
+			std::scoped_lock _(m_frame_registry_lock);
+			ret_val.data.index = m_frame_buffer_registry.size();
+			CommandBuffer_VK& cmd_buffer_internal = &m_frame_buffer_registry.emplace_back();
+
+			uint32_t cmd_current = cmd_count++;
+			if (cmd_current >= frame_command_pool.size())
+			{
+				commandlists.push_back(std::make_unique<CommandList_Vulkan>());
+			}
+		}
+
+		size_t index = frame_command_pool.num_active_buffers++;
 
 		if (m_frame_command_buffers.size() < index)
 			m_frame_command_buffers.emplace_back();
