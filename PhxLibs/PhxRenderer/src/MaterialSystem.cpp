590307767703d84b88bef45efe3b8c3ab26b63c9
dipaolo21 <dipaolo21@gmail.com>
2026-04-01 21:59:39 -0400

    Working on material system still - this is going to be a doozy.

--- PhxLibs/PhxRenderer/src/MaterialSystem.cpp
+++ PhxLibs/PhxRenderer/src/MaterialSystem.cpp
@@ -9,6 +9,9 @@
 #include <PhxAsset/AssetDatabase.h>
 #include <PhxRenderer/Shaders/ShaderSystem.h>
 
+#include <PhxRenderer/TextureResource.h>
+#include <PhxResource/ResourceManager.h>
+
 // -- TODO: Clean API Leak at some point ---
 #include <slang.h>
 
@@ -17,11 +20,19 @@ using namespace phx::renderer;
 
 namespace
 {
+    constexpr const char* kMaterialDataStructName = "MaterialData";
+
     struct MtlArchetypeSlang
     {
-        phx::renderer::ShaderDescriptor base_shader_desc;
-        bool is_double_sided;
+        std::string virtual_path;
         phx::MemoryBuffer default_instance_data;
+        struct TextureBinding
+        {
+            ShaderFieldDesc field_desc;
+            RefCountPtr<TextureResource> default_texture;
+        };
+
+        std::vector<TextureBinding> default_textures;
     };
 
     struct MtlInstanceImpl
@@ -33,7 +44,7 @@ namespace
     phx::IVirtualFileSystem* g_vfs;
     phx::PagedPool<MaterialArchetype, MtlArchetypeSlang> g_archetype_pool;
     phx::PagedPool<MaterialInstance, MtlInstanceImpl> g_instance_pool;
-    std::unordered_map<MtlArchetypeHandle, std::string> g_archetype_lut;
+    std::unordered_map<std::string, MtlArchetypeHandle> g_archetype_lut;
 }
 
 void phx::renderer::MaterialSystem::Initialize(IVirtualFileSystem *vfs, uint32_t max_archetypes, uint32_t max_instances)
@@ -51,7 +62,6 @@ void phx::renderer::MaterialSystem::RegisterArchetypes(Span<std::string> virtual
 {
     for (auto& virtual_path : virtual_paths)
     {
-        
         phx::asset::Ptr<asset::MaterialArchetypeDef> def = phx::asset::AssetDB::Get<asset::MaterialArchetypeDef>(virtual_path);
         if (def)
             CreateArchetype(virtual_path, *def);
@@ -60,18 +70,90 @@ void phx::renderer::MaterialSystem::RegisterArchetypes(Span<std::string> virtual
 
 MtlArchetypeHandle phx::renderer::MaterialSystem::CreateArchetype(std::string virtual_path, const asset::MaterialArchetypeDef& def)
 {
+    auto itr = g_archetype_lut.find(virtual_path);
+    if (itr != g_archetype_lut.end())
+        return itr->second;
+
     const bool registered = ShaderSystem::RegisterModule(def.shader_desc.source);
     if (!registered)
     {
-        PHX_LOG_ERROR("Failed to register shader module for material archetype '{}'", virtual_path);
+        PHX_CORE_ERROR("Failed to register shader module for material archetype '{0}'", virtual_path.c_str());
         return MtlArchetypeHandle::CreateInvalid();
     }
 
-    return MtlArchetypeHandle();
+    ShaderStructDesc material_struct_desc;
+
+    if (!ShaderSystem::FindStruct(def.shader_desc.source, kMaterialDataStructName, material_struct_desc))
+    {
+        PHX_CORE_ERROR(
+            "Failed to find '{0}' struct in shader module for material archetype '{1}'",
+            kMaterialDataStructName,
+            virtual_path.c_str());
+        return MtlArchetypeHandle::CreateInvalid();
+    }
+    
+    PHX_ASSERT(material_struct_desc.size == 256, "MaterialData struct must be 256 bytes in size");
+
+    MtlArchetypeHandle archetype_handle = g_archetype_pool.Allocate();
+    PHX_CORE_ASSERT(archetype_handle.IsValid(), "Failed to acquire material archetype handle.");
+
+    MtlArchetypeSlang& archetype = *g_archetype_pool.GetHot(archetype_handle);
+        struct ShaderDescriptor
+    {
+        std::string virtual_path;
+
+        struct GenericArg
+        {
+            std::string name;
+            std::string value;
+
+            bool is_type = false;
+        };
+
+        std::vector<GenericArg> generic_args;
+        std::vector<ShaderEntryPoint> entry_points;
+
+        Hash64 GetHash() const;
+    };
+
+
+    archetype.virtual_path = def.shader_desc.source;
+    archetype.default_instance_data = phx::MemoryBuffer(material_struct_desc.size);
+    TypedView<uint8_t> instance_data_view = archetype.default_instance_data.GetView<uint8_t>();
+
+    for (auto& param : def.params)
+    {
+        for (auto& field : material_struct_desc.fields)
+        {
+            if (field.name != phx::StringHash(param.name))
+                continue;
+
+            std::visit([&](auto &&arg)
+            {
+                using T = std::decay_t<decltype(arg)>;
+                
+                if constexpr (std::is_same_v<T, rfl::Field<"texture", std::string>>) 
+                {
+                    RefCountPtr<TextureResource> texture = ResourceManager::Load<TextureResource>(arg.value().c_str());
+                    archetype.default_textures.push_back({ field, texture });
+                }
+                else 
+                {
+                    PHX_CORE_ASSERT(field.size == sizeof(T::Type), "Field size mismatch for float4");
+                    uint8_t* entry = instance_data_view.Get() + field.offset;
+                    std::memcpy(entry, &arg.value(), field.size);
+                }
+            }, param.value);
+        }
+    }
+
+    g_archetype_lut[virtual_path] = archetype_handle;
+    return archetype_handle;
 }
 
 MtlInstanceHandle phx::renderer::MaterialSystem::CreateInstance(const asset::MaterialInstanceDef &def)
 {
+    // TODO: I am here.
     return MtlInstanceHandle();
 }
 
--- PhxLibs/PhxRenderer/src/Shaders/ShaderSystem.cpp
+++ PhxLibs/PhxRenderer/src/Shaders/ShaderSystem.cpp
@@ -20,6 +20,7 @@ namespace
     {
         std::string source_path;
         Slang::ComPtr<slang::IModule> slang_module;
+        std::unordered_map<std::string, ShaderStructDesc> type_desc_cache;
     };
 
     struct SlangShader
@@ -151,24 +152,38 @@ bool phx::renderer::ShaderSystem::FindStruct(const std::string &virtual_path, st
     if (itr == g_module_cache.end())
         return false;
 
-    slang::ProgramLayout* program_layout = itr->second.slang_module->getLayout();
-    PHX_ASSERT(program_layout);
-
-    slang::TypeReflection* type_reflection = program_layout->findTypeByName(struct_name);
-    if (!type_reflection)
-        return false;
-
-    // TODO: Cache this
-
-    slang::TypeLayoutReflection* type_layout = program_layout->getTypeLayout(type_reflection, slang::LayoutRules::Default);
-
-    // I AM HERE: FILL IN DESC>
-    for (uint32_t i = 0; i < type_reflection->getFieldCount(); ++i)
+    auto emplace_result =itr->second.type_desc_cache.try_emplace(struct_name, [&]()
     {
-        ShaderFieldDesc field_desc;
-    }
+        slang::ProgramLayout* program_layout = itr->second.slang_module->getLayout();
+        PHX_ASSERT(program_layout);
 
-    ShaderStructDesc desc = {};
+        slang::TypeReflection* type_reflection = program_layout->findTypeByName(struct_name);
+        if (!type_reflection)
+            return false;
 
-    return true;
+        // TODO: Cache this
+
+        slang::TypeLayoutReflection* type_layout = program_layout->getTypeLayout(type_reflection, slang::LayoutRules::Default);
+
+        ShaderStructDesc desc = {
+            .name = StringHash(struct_name),
+            .size = (uint32_t)type_layout->getSize(),
+        };
+        
+        desc.fields.resize(type_reflection->getFieldCount());
+        for (uint32_t i = 0; i < type_reflection->getFieldCount(); ++i)
+        {
+            auto field_reflection = type_reflection->getFieldByIndex(i);
+            auto field_layout = type_layout->getFieldByIndex(i);
+
+            desc.fields[i] = {
+                .name = StringHash(field_reflection->getName()),
+                .offset = static_cast<uint32_t>(field_layout->getOffset()),
+                .size = static_cast<uint32_t>(field_layout->getSize()),
+            };
+        }
+    });
+    
+    out_desc = emplace_result.first->second;
+    return emplace_result.second;
 }
