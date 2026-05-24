a391eb27365ad61c359ade8c206d22d8f93b9bcc
cdipaolo <dipaolo21@gmail.com>
2026-05-24 07:08:40 -0400

    iimpl v mem stuff

---
+++ PhxEngine/Platform/Windows/VirtualMemory.cpp
@@ -0,0 +1,38 @@
+#include <PhxEngine/Platform/VirtualMemory.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+namespace phx;
+
+namespace platform::VirtualMemory
+{
+    usize GetPageSize()
+    {
+        SYSTEM_INFO sys_info;
+        GetSystemInfo(&sys_info);
+        return sys_info.dwPageSize;
+    }
+
+    usize GetAllocationGranularity()
+    {
+        SYSTEM_INFO sys_info;
+        GetSystemInfo(&sys_info);
+        return sys_info.dwAllocationGranularity;
+    }
+
+    void* Reserve(usize size)
+    {
+        return VirtualAlloc(nullptr, size, MEM_RESERVE, PAGE_NOACCESS);
+    }
+
+    bool Commit(void* ptr, usize size)
+    {
+        return VirtualAlloc(ptr, size, MEM_COMMIT, PAGE_READWRITE) != nullptr;
+    }
+
+    void Release(void* ptr, usize size)
+    {
+        VirtualFree(ptr, 0, MEM_RELEASE);
+    }
+}
