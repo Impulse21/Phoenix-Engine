1f743991b526772e31e7d6ca1f4d634d73421fa9
Impulse21 <dipaolo21@gmail.com>
2026-06-13 19:13:30 -0400

    Impl swapchain creation

---
+++ PhxEngine/RHI/Vulkan/VulkanResources.cpp
@@ -0,0 +1,17 @@
+#include <PhxEngine/RHI/RHI.h>
+
+#include "RHIVulkan.h"
+
+using namespace phx;
+using namespace phx::rhi;
+using namespace phx::rhi::vulkan;
+
+SwapchainHandle phx::rhi::CreateSwapchain(const SwapchainDesc& desc)
+{
+
+}
+
+void phx::rhi::DeleteSwapchain(SwapchainHandle handle)
+{
+
+}
